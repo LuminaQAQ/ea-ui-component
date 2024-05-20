38c10ff2ae6843d84760c01352337dd96fa8ac0e
LuminaQAQ <1710884533@qq.com>
2024-05-20 17:12:20 +0800

    feat: 细微更新

--- components/ea-button/ea-button.js
+++ components/ea-button/ea-button.js
@@ -242,17 +242,49 @@ export default class EaButton extends HTMLElement {
     shadowRoot.appendChild(test);
   }
 
+
+  get BUTTON_STYLE() {
+    return ['plain', 'round'];
+  }
+
+  // ------- 禁用 -------
+  // #region
   get disabled() {
     return this.getAttribute('disabled') !== null;
   }
 
-  get checked() {
-    return this.getAttribute('checked');
+  get ariaDisabled() {
+    return this.getAttribute('aria-disabled') !== null;
+  }
+  // #endregion
+  // ------- end -------
+
+  // ------- 按钮内容 -------
+  // #region
+  get value() {
+    return this.getAttribute('value');
   }
 
-  get circle() {
-    return this.getAttribute('circle') !== null;
+  get innerHTMLValue() {
+    return this.innerHTML;
   }
+
+  get ariaValueText() {
+    return this.getAttribute('aria-valuetext');
+  }
+  // #endregion
+  // ------- end -------
+
+  // ------- 按钮样式 -------
+  // #region
+  get plain() {
+    return this.getAttribute('plain') !== undefined && this.getAttribute('plain') !== null;
+  }
+  get round() {
+    return this.getAttribute('round') !== undefined && this.getAttribute('round') !== null;
+  }
+  // #endregion
+  // ------- end -------
 
   get type() {
     const attr = this.getAttribute('type');
@@ -263,8 +295,24 @@ export default class EaButton extends HTMLElement {
   connectedCallback() {
     const button = this.shadowRoot.querySelector('.__ea-button');
 
+    // 禁用
     button.disabled = this.disabled;
-    if (this.disabled) button.classList.add('disabled');
+    button.ariaDisabled = this.ariaDisabled;
+
+    // 按钮内容
+    button.ariaValueText = this.ariaValueText;
+    button.value = this.value;
+    button.value = this.innerHTMLValue;
+
+    // 按钮样式
+    for (let i = 0, style; style = this.BUTTON_STYLE[i++];) {
+      if (this[style]) {
+        button.classList.add(style);
+        break;
+      }
+    }
+
+    if (this.disabled || this.ariaDisabled) button.classList.add('disabled');
     button.classList.add(this.type);
   }
 }
