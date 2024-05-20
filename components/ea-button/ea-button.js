e6394a709b76cfa480a06ada91b9b7d86f1638b1
LuminaQAQ <1710884533@qq.com>
2024-05-20 16:22:09 +0800

    :sparkle: add: 添加了 按钮样式类属性及其样式

--- components/ea-button/ea-button.js
+++ components/ea-button/ea-button.js
@@ -1,72 +1,29 @@
 // @ts-nocheck
 import setStyle from "../../utils/setStyle";
 
-const styleSheet = `
-.__ea-button {
-  padding: 0.25rem 1rem;
-  cursor: pointer;
-  font-size: 1.25rem;
-  line-height: 1.25;
-  font-weight: 500;
-  transition: background-color 0.1s, color 0.1s;
-  border-radius: 5px;
-}
-.__ea-button.normal {
-  border: 1px solid #e6e6e6;
-  color: black;
-  background-color: transparent;
-}
-.__ea-button.normal.disabled {
-  cursor: not-allowed !important;
-  background-image: none !important;
-  background-color: rgba(64, 64, 64, 0) !important;
-  border-color: white !important;
-  color: #404040 !important;
-}
-.__ea-button.normal:hover {
-  border: 1px solid rgba(160, 207, 255, 0.4);
-  color: #3a9bff;
-  background-color: rgba(160, 207, 255, 0.05);
-}
-.__ea-button.primary {
-  border: 1px solid #409eff;
-  color: #ffffff;
-  background-color: #409eff;
-}
-.__ea-button.primary.disabled {
-  cursor: not-allowed !important;
-  background-image: none !important;
-  background-color: #c0dfff !important;
-  border-color: #c0dfff !important;
-  color: white !important;
-}
-.__ea-button.primary:hover {
-  border: 1px solid #a5d2ff;
-  color: #ffffff;
-  background-color: #a5d2ff;
-}
-`
-
 export default class EaButton extends HTMLElement {
+
   constructor() {
     super();
 
     const shadowRoot = this.attachShadow({ mode: 'open' });
-
 
     const test = document.createElement('input');
     test.type = 'button';
     test.value = "click";
     test.className = "__ea-button";
 
-    setStyle(shadowRoot, new URL('./index.css', import.meta.url).href);
-    // const style = document.createElement('style');
-    // style.textContent = styleSheet;
-    // shadowRoot.appendChild(style);
 
+    setStyle(shadowRoot, new URL('./index.css', import.meta.url).href);
     shadowRoot.appendChild(test);
   }
 
+  get BUTTON_STYLE() {
+    return ['plain', 'round'];
+  }
+
+  // ------- 禁用 -------
+  // #region
   get disabled() {
     return this.getAttribute('disabled') !== null;
   }
@@ -74,14 +31,35 @@ export default class EaButton extends HTMLElement {
   get ariaDisabled() {
     return this.getAttribute('aria-disabled') !== null;
   }
+  // #endregion
+  // ------- end -------
 
-  get checked() {
-    return this.getAttribute('checked');
+  // ------- 按钮内容 -------
+  // #region
+  get value() {
+    return this.getAttribute('value');
   }
 
-  get circle() {
-    return this.getAttribute('circle') !== null;
+  get innerHTMLValue() {
+    return this.innerHTML;
   }
+
+  get ariaValueText() {
+    return this.getAttribute('aria-valuetext');
+  }
+  // #endregion
+  // ------- end -------
+
+  // ------- 按钮样式 -------
+  // #region
+  get plain() {
+    return this.getAttribute('plain') !== undefined && this.getAttribute('plain') !== null;
+  }
+  get round() {
+    return this.getAttribute('round') !== undefined && this.getAttribute('round') !== null;
+  }
+  // #endregion
+  // ------- end -------
 
   get type() {
     const attr = this.getAttribute('type');
@@ -92,8 +70,22 @@ export default class EaButton extends HTMLElement {
   connectedCallback() {
     const button = this.shadowRoot.querySelector('.__ea-button');
 
+    // 禁用
     button.disabled = this.disabled;
     button.ariaDisabled = this.ariaDisabled;
+
+    // 按钮内容
+    button.ariaValueText = this.ariaValueText;
+    button.value = this.value;
+    button.value = this.innerHTMLValue;
+
+    // 按钮样式
+    for (let i = 0, style; style = this.BUTTON_STYLE[i++];) {
+      if (this[style]) {
+        button.classList.add(style);
+        break;
+      }
+    }
 
     if (this.disabled || this.ariaDisabled) button.classList.add('disabled');
     button.classList.add(this.type);
