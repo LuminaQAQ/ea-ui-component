b7e30f39ecba09b6e9b406a0e21acc5289b1b482
LuminaQAQ <1710884533@qq.com>
2024-05-21 00:38:04 +0800

    Updates

--- assets/example.md.Oi8po2XR.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,o as i,c as a,a2 as t}from"./chunks/framework.cWN-w6jb.js";const y=JSON.parse('{"title":"快速上手","description":"","frontmatter":{"outline":"deep"},"headers":[],"relativePath":"example.md","filePath":"example.md"}'),n={name:"example.md"},h=t("",4),l=[h];function k(p,e,E,r,d,g){return i(),a("div",null,l)}const c=s(n,[["render",k]]);export{y as __pageData,c as default};
