32ea4f9ea36dd077a41b7da569ce954fdc379acf
LuminaQAQ <1710884533@qq.com>
2024-05-21 00:38:04 +0800

    Updates

---
+++ assets/test.md.DVH3tm8Q.lean.js
@@ -0,0 +1,4 @@
+import{y as l,a1 as t,D as h,o as a,c as p,b as k,j as e,a as s,I as E,w as r,a3 as d,a2 as o}from"./chunks/framework.CSjtZPXU.js";const _={class:"row left"},g=o("",1),F=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"test.md","filePath":"test.md"}'),c={name:"test.md"},v=Object.assign(c,{setup(y){return l(()=>{t(()=>import("./chunks/index.BfzKuk1m.js").then(i=>i.i),[]),t(()=>import("./chunks/index.l0sNRNKZ.js"),[]),t(()=>import("./chunks/index.BfzKuk1m.js").then(i=>i.e),[])}),(i,u)=>{const n=h("ea-button");return a(),p("div",null,[(a(),k(d,null,[e("pre",null,[s("    "),e("div",_,[s(`
+        `),E(n,{type:"primary",loading:""},{default:r(()=>[s("文字按钮")]),_:1}),s(`
+    `)]),s(`
+`)])],1024)),g])}}});export{F as __pageData,v as default};
