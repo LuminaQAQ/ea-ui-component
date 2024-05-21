cbefd2e626cbcc0952778996e1b8c469e8c2b3a6
LuminaQAQ <1710884533@qq.com>
2024-05-21 16:07:59 +0800

    :sparkles: add: 添加了 icon 组件

--- components/ea-button-group/index.js
+++ components/ea-button-group/index.js
@@ -446,39 +446,35 @@ const stylesheet = `
 
 export default class EaButtonGroup extends HTMLElement {
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        const shadowRoot = this.attachShadow({ mode: 'open' });
+    const shadowRoot = this.attachShadow({ mode: 'open' });
 
-        const dom = document.createElement('div');
-        shadowRoot.appendChild(dom);
-        const slot = document.createElement('slot');
-        dom.className = "__ea-button-group";
-        dom.appendChild(slot);
-
-
-        this.dom = dom;
-
-        // ------- 打包 -------
-        // #region
-        const styleNode = document.createElement('style');
-        styleNode.innerHTML = stylesheet;
-        this.shadowRoot.appendChild(styleNode);
-        // #endregion
-        // ------- end -------
-
-        // ------- 本地调试 -------
-        // #region
-        // setStyle(shadowRoot, new URL('./index.css', import.meta.url).href);
-        // #endregion
-        // ------- end -------
+    const dom = document.createElement('div');
+    shadowRoot.appendChild(dom);
+    const slot = document.createElement('slot');
+    dom.className = "__ea-button-group";
+    dom.appendChild(slot);
 
 
-        shadowRoot.appendChild(dom);
-    }
-}
+    this.dom = dom;
 
-if (!window.customElements.get("ea-buttonn-group")) {
-    window.customElements.define("ea-button-group", EaButtonGroup);
+    // ------- 打包 -------
+    // #region
+    const styleNode = document.createElement('style');
+    styleNode.innerHTML = stylesheet;
+    this.shadowRoot.appendChild(styleNode);
+    // #endregion
+    // ------- end -------
+
+    // ------- 本地调试 -------
+    // #region
+    // setStyle(shadowRoot, new URL('./index.css', import.meta.url).href);
+    // #endregion
+    // ------- end -------
+
+
+    shadowRoot.appendChild(dom);
+  }
 }
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+// @ts-nocheck
 import EaButton from "./components/ea-button/ea-button";
 import EaButtonGroup from "./components/ea-button-group/";
+
+
+
+if (!window.customElements.get("ea-button")) {
+    window.customElements.define("ea-button", EaButton);
+    window.customElements.define("ea-button-group", EaButtonGroup);
+}
