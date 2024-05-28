baf846034fff27eefa88c6a9a169796a59e91342
LuminaQAQ <1710884533@qq.com>
2024-05-29 02:41:08 +0800

    :sparkles: add: 添加了多选框的 禁用状态 :sparkles: initial: 初始化了 多选框组 组件

---
+++ components/ea-checkbox-group/index.js
@@ -0,0 +1,63 @@
+// @ts-nocheck
+import setStyle from "../../utils/setStyle";
+
+const stylesheet = ``;
+
+export default class EaCheckboxGroup extends HTMLElement {
+
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        const dom = document.createElement('div');
+        shadowRoot.appendChild(dom);
+        const slot = document.createElement('slot');
+        dom.className = "ea-checkbox-group";
+        dom.appendChild(slot);
+
+
+        this.dom = dom;
+
+        // ------- 打包 -------
+        // #region
+        // const styleNode = document.createElement('style');
+        // styleNode.innerHTML = stylesheet;
+        // this.shadowRoot.appendChild(styleNode);
+        // #endregion
+        // ------- end -------
+
+        // ------- 本地调试 -------
+        // #region
+        setStyle(shadowRoot, new URL('./index.css', import.meta.url).href);
+        // #endregion
+        // ------- end -------
+
+        shadowRoot.appendChild(dom);
+
+        console.log();
+    }
+
+    // ------- name 唯一键值 -------
+    // #region
+    get name() {
+        return this.getAttribute('name');
+    }
+
+    set name(val) {
+        this.querySelectorAll('ea-checkbox').forEach(checkbox => {
+            checkbox.setAttribute('name', val);
+        });
+    }
+    // #endregion
+    // ------- end -------
+
+    init() {
+        // name 唯一键值
+        this.name = this.name;
+    }
+
+    connectedCallback() {
+        this.init();
+    }
+}
