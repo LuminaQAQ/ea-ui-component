29e34b03e187b51167b1db67d86130715521a49d
LuminaQAQ <1710884533@qq.com>
2024-06-18 03:35:48 +0800

    :sparkles: feat: 完成了 ea-badge 组件

--- components/ea-badge/index.js
+++ components/ea-badge/index.js
@@ -3,6 +3,48 @@ import Base from '../Base';
 
 const stylesheet = `
 @import url('/ea_ui_component/icon/index.css');
+
+.ea-badge_wrap {
+  position: relative;
+  vertical-align: middle;
+  display: inline-block;
+}
+.ea-badge_wrap .ea-badge_content {
+  display: inline-block;
+  padding: 0 0.375rem;
+  border-radius: 0.625rem;
+  border: 1px solid #fff;
+  height: 1.125rem;
+  line-height: 1.125rem;
+  position: absolute;
+  right: 0.625rem;
+  top: 0;
+  transform: translate(100%, -50%);
+  color: #fff;
+  font-size: 0.75rem;
+  text-align: center;
+  white-space: nowrap;
+  background-color: #f56c6c;
+}
+.ea-badge_wrap .ea-badge_content.primary {
+  background-color: #409eff;
+}
+.ea-badge_wrap .ea-badge_content.success {
+  background-color: #67c23a;
+}
+.ea-badge_wrap .ea-badge_content.warning {
+  background-color: #e6a23c;
+}
+.ea-badge_wrap .ea-badge_content.info {
+  background-color: #909399;
+}
+.ea-badge_wrap .ea-badge_content.dot {
+  right: 0.3125rem;
+  padding: 0;
+  border-radius: 50%;
+  width: 0.5rem;
+  height: 0.5rem;
+}
 `;
 
 export default class EaBadge extends Base {
@@ -78,6 +120,21 @@ export default class EaBadge extends Base {
     // #endregion
     // ------- end -------
 
+    // ------- is-dot 是否为点状徽章 -------
+    // #region
+    get isDot() {
+        return this.getAttrBoolean('is-dot') || false;
+    }
+
+    set isDot(value) {
+        this.toggleAttr('is-dot', value);
+
+        this.#badgeWrap.innerText = value ? '' : this.value;
+        this.#badgeWrap.classList.toggle('dot', value);
+    }
+    // #endregion
+    // ------- end -------
+
     init() {
         const that = this;
 
@@ -86,6 +143,8 @@ export default class EaBadge extends Base {
         this.type = this.type;
 
         this.max = this.max;
+
+        this.isDot = this.isDot;
 
         try {
             const button = this.querySelector('ea-button');
