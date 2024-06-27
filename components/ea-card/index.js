a3bb9bfa82ff440852f4f38d549d03ef57cbdc10
LuminaQAQ <1710884533@qq.com>
2024-06-27 16:16:02 +0800

    :pencil: docs: update

--- components/ea-card/index.js
+++ components/ea-card/index.js
@@ -4,12 +4,28 @@ import { createElement, createSlotElement } from '../../utils/createElement.js';
 
 const stylesheet = `
 @import url('/ea_ui_component/icon/index.css');
+
+.ea-card_wrap {
+  border-radius: 4px;
+  border: 1px solid #ebeef5;
+  background-color: #fff;
+  overflow: hidden;
+  color: #303133;
+  transition: box-shadow 0.3s;
+}
+.ea-card_wrap.is-always-shadow, .ea-card_wrap.is-hover-shadow:hover {
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+}
+.ea-card_wrap.is-never-shadow {
+  box-shadow: none;
+}
+.ea-card_wrap .ea-card_content {
+  padding: 20px;
+}
 `;
 
 export class EaCard extends Base {
     #wrap;
-
-
 
     constructor() {
         super();
