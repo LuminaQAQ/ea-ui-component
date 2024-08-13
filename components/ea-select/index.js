b4eab532bf77970079ad5953c20d611e41617cf4
LuminaQAQ <1710884533@qq.com>
2024-08-14 01:00:36 +0800

    :sparkles: feat: Ea-option: 添加自定义事件 + name属性

--- components/ea-select/index.js
+++ components/ea-select/index.js
@@ -74,7 +74,7 @@ export class EaSelect extends Base {
                 <div class="ea-select_input-wrap">
                     <ea-input type="text" part="input" readonly autocomplete="off"></ea-input>
                     <span class="ea-select_dropdown-icon">
-                        <i part="icon" class="icon-angle-down" style="color: #c0c4cc;"></i>
+                        <ea-icon part="icon" icon="icon-angle-down" color="#c0c4cc"></ea-icon>
                     </span>
                 </div>
                 <div class="ea-select_dropdown-wrap">
@@ -95,6 +95,18 @@ export class EaSelect extends Base {
 
         this.build(shadowRoot, stylesheet);
     }
+
+    // ------- name 若与 form 组合使用，则 form 的返回值中. 该键名为该name -------
+    // #region
+    get name() {
+        return this.getAttribute('name') || 'select';
+    }
+
+    set name(val) {
+        this.setAttribute('name', val);
+    }
+    // #endregion
+    // ------- end -------
 
     // ------- width 输入框宽度 -------
     // #region
@@ -172,17 +184,23 @@ export class EaSelect extends Base {
 
         this.setAttribute('clearable', val);
 
-        const icon = this.#dropdownIcon.querySelector('ea-icon');
+        const icon = this.#dropdownIcon.querySelector('i');
 
         this.#dropdownIcon.addEventListener('mouseenter', (e) => {
-            icon.icon = 'icon-cancel-circled2';
+            icon.className = 'icon-cancel-circled2';
         });
 
         this.#dropdownIcon.addEventListener('mouseleave', (e) => {
-            icon.icon = 'icon-angle-down';
+            icon.className = 'icon-angle-down';
         });
 
         this.#dropdownIcon.addEventListener('click', (e) => {
+            this.dispatchEvent(new CustomEvent('clear', {
+                detail: {
+                    originValue: this.selection
+                }
+            }));
+
             this.selection = '';
         });
     }
@@ -256,6 +274,11 @@ export class EaSelect extends Base {
                         });
                         option.checked = true;
                     }
+                    this.dispatchEvent(new CustomEvent('change', {
+                        detail: {
+                            value: this.selection
+                        }
+                    }));
                 });
             }
         });
@@ -264,6 +287,12 @@ export class EaSelect extends Base {
     #initDropdownWrapOpen() {
         this.#selectInput.addEventListener('focus', (e) => {
             this.#container.classList.add('is-open');
+
+            this.dispatchEvent(new CustomEvent('visible-change', {
+                detail: {
+                    visible: true,
+                }
+            }));
         });
 
         this.#selectInput.addEventListener('blur', (e) => {
@@ -272,12 +301,21 @@ export class EaSelect extends Base {
                 timer = null;
                 this.#container.classList.remove('is-open');
             }, 100);
+
+            this.dispatchEvent(new CustomEvent('visible-change', {
+                detail: {
+                    visible: false,
+                }
+            }));
         });
     }
+
 
     #init() {
         this.setAttribute('data-ea-component', true);
         this.#dropdownWrap.style.width = this.width;
+
+        this.name = this.name;
 
         this.width = this.width;
 
