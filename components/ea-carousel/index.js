9dd5a406f682eee607bce2cc82584d577d196db0
LuminaQAQ <1710884533@qq.com>
2024-09-03 02:25:41 +0800

    refactor(ea-carousel): 将实用程序函数内部的逻辑移动到组件逻辑中
    将`createArrowItem`、`initIndicators`和`initCarouselAutoPlay`实用程序的逻辑重写并集成到组件的本地逻辑中，以提升代码的模块化和可维护性。删除了不必要的导入并直接实现了功能，以提高代码的自足性和清晰度。

--- components/ea-carousel/index.js
+++ components/ea-carousel/index.js
@@ -1,11 +1,9 @@
-// @ts-nocheck
 import Base from '../Base.js';
 import "../ea-carousel-item/index.js"
 
-import { createArrowItem } from './src/utils/createArrowItem.js';
+import { createElement } from '../../utils/createElement.js';
+
 import { handleIndexOverflow } from './src/utils/handleIndexOverflow.js';
-import { initIndicators } from './src/utils/initIndicators.js';
-import { initCarouselAutoPlay } from './src/utils/initCarouselAutoPlay.js';
 
 import { stylesheet } from './src/style/stylesheet.js'
 
@@ -22,10 +20,10 @@ export class EaCarousel extends Base {
         const shadowRoot = this.attachShadow({ mode: 'open' });
         shadowRoot.innerHTML = `
             <div class='ea-carousel_wrap' part='container'>
-                <div class='ea-carousel_content-container' part='content-container'>
+                <div class='ea-carousel_content-container' part='content-wrap'>
                     <slot></slot>
                 </div>
-                <div class='ea-carousel-indicator_wrap' part='indicator-container'></div>
+                <div class='ea-carousel-indicator_wrap' part='indicator-wrap'></div>
             </div>
         `;
 
@@ -118,6 +116,77 @@ export class EaCarousel extends Base {
     // #endregion
     // ------- end -------
 
+    #initIndicators() {
+        const length = this.querySelectorAll('ea-carousel-item').length;
+
+        for (let i = 0; i < length; i++) {
+            const indicator = createElement('div', 'ea-carousel-item_indicator');
+            this.#indicatorWrap.appendChild(indicator);
+        }
+
+        const indicators = this.#indicatorWrap.querySelectorAll('.ea-carousel-item_indicator');
+        indicators[0].classList.add('ea-carousel-item_indicator--active');
+        indicators.forEach((indicator, index) => {
+            indicator.addEventListener(this.trigger === 'click' ? 'click' : 'mouseenter', () => {
+                this.index = index;
+
+                indicators.forEach((item) => {
+                    item.classList.remove('ea-carousel-item_active');
+                })
+                indicator.classList.add('ea-carousel-item_active');
+            })
+        });
+    }
+
+    #initCarouselAutoPlay() {
+        let timer = setInterval(() => {
+            this.index = this.index + 1;
+        }, this.interval * 1000);
+
+        // 当鼠标进入轮播图区域时，停止自动播放
+        this.addEventListener('mouseenter', () => {
+            clearInterval(timer);
+            timer = null;
+        });
+
+        // 当鼠标离开轮播图区域时，恢复自动播放
+        this.addEventListener('mouseleave', () => {
+            timer = setInterval(() => {
+                this.index = this.index + 1;
+            }, this.interval * 1000)
+        });
+    }
+
+    #createArrowItem(arrow) {
+        let throttle = false;
+        const arrowItem = createElement('div', `ea-carousel-item_arrow ea-carousel-item_arrow--${arrow}`);
+
+        arrowItem.innerHTML = this.arrow === "left" ? `&lt;` : `&gt;`;
+
+        switch (this.arrow) {
+            case "always":
+                this.#container.classList.add('always-show-arrow');
+                break;
+            case "hover":
+                this.#container.classList.add('hover-trigger');
+                break;
+        }
+
+        arrowItem.addEventListener('click', () => {
+            if (throttle) return;
+            this.index = this.arrow === "left" ? --this.index : ++this.index;
+        })
+
+        this.#contentContainer.addEventListener('transitionstart', () => {
+            throttle = true;
+        })
+        this.#contentContainer.addEventListener('transitionend', () => {
+            throttle = false;
+        })
+
+        return arrowItem;
+    }
+
     connectedCallback() {
         this.direction = this.direction;
 
@@ -129,13 +198,12 @@ export class EaCarousel extends Base {
 
         this.index = this.index;
 
-        initIndicators.call(this, this.#indicatorWrap, this.querySelectorAll('ea-carousel-item').length);
-
-        initCarouselAutoPlay.call(this, this.interval);
+        this.#initIndicators();
+        this.#initCarouselAutoPlay();
 
         if (this.arrow !== "never" || this.direction !== "vertical") {
-            const leftArrowItem = createArrowItem.call(this, this.#container, this.#contentContainer, "left");
-            const rightArrowItem = createArrowItem.call(this, this.#container, this.#contentContainer, "right");
+            const leftArrowItem = this.#createArrowItem("left");
+            const rightArrowItem = this.#createArrowItem("right");
 
             this.#container.appendChild(leftArrowItem);
             this.#container.appendChild(rightArrowItem);
