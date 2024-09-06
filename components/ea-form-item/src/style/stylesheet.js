5a83acfd69af783c170b654c5f93e28540fdbaa4
LuminaQAQ <1710884533@qq.com>
2024-09-07 00:39:16 +0800

    fix: EaForm: - 修复了验证错误的样式丢失的问题

--- components/ea-form-item/src/style/stylesheet.js
+++ components/ea-form-item/src/style/stylesheet.js
@@ -37,6 +37,17 @@ export const stylesheet = `
 .ea-form-item_wrap.is-required .ea-form-item_content-wrap .ea-form-item_invalid-wrap {
   transform: translateY(100%) scaleY(1);
 }
+.ea-form-item_wrap.is-required .ea-form-item_label-wrap {
+  color: #f56c6c;
+}
+.ea-form-item_wrap.is-required ::slotted(ea-input),
+.ea-form-item_wrap.is-required ::slotted(ea-select),
+.ea-form-item_wrap.is-required ::slotted(ea-textarea),
+.ea-form-item_wrap.is-required ::slotted(ea-time-picker),
+.ea-form-item_wrap.is-required ::slotted(ea-date-picker) {
+  border: 1px solid #f56c6c;
+  border-radius: 3px;
+}
 .ea-form-item_wrap.with-transition .ea-form-item_content-wrap .ea-form-item_invalid-wrap {
   transition: transform 0.3s;
 }
--- components/ea-radio-group/index.js
+++ components/ea-radio-group/index.js
@@ -1,4 +1,5 @@
 // @ts-nocheck
+import { timeout } from "../../utils/timeout.js";
 import Base from "../Base.js";
 
 const stylesheet = `
