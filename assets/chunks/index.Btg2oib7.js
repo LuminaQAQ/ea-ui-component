0df8cf1cd42a3e638c8cb8aa8f2481c744f75f9a
LuminaQAQ <1710884533@qq.com>
2024-09-08 18:32:27 +0800

    Updates

---
+++ assets/chunks/index.Btg2oib7.js
@@ -0,0 +1,54 @@
+var m=(s,t,e)=>{if(!t.has(s))throw TypeError("Cannot "+e)};var o=(s,t,e)=>(m(s,t,"read from private field"),e?e.call(s):t.get(s)),l=(s,t,e)=>{if(t.has(s))throw TypeError("Cannot add the same private member more than once");t instanceof WeakSet?t.add(s):t.set(s,e)},h=(s,t,e,n)=>(m(s,t,"write to private field"),n?n.call(s,e):t.set(s,e),e);import{B as f}from"./Base.LSgLRpFA.js";import"./index.CgEiilRo.js";class u{handleStringMsg(t,e){t.text=e,t.type="info",t.hasClose=!1}handleObjectMsg(t,e,n){for(const i in e)n.includes(i)&&(t[i]=e[i]);Object.keys(e).includes("type")||(t.type="info")}handleDuration(t,e=3){if(e===0)return;let n=setTimeout(()=>{t.show=!1,clearTimeout(n),n=null},e*1e3+40)}open(t){const e=document.createElement("ea-message");if(document.body.appendChild(e),typeof t=="string")this.handleStringMsg(e,t),this.handleDuration(e);else if(typeof t=="object")this.handleObjectMsg(e,t,e.attrs),this.handleDuration(e,t.duration);else throw new Error("[EaMessage] TypeError");return e.show=!0,{onClose(n){typeof n=="function"&&e.addEventListener("click",function(){n()})}}}}const w=`
+.ea-message_wrap {
+  position: fixed;
+  left: 50%;
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  padding: 15px 15px 15px 20px;
+  border: 1px solid #ebeef5;
+  border-radius: 4px;
+  top: -100%;
+  transform-origin: center;
+  opacity: 0;
+  transform: translate(-50%, 0);
+  min-width: 380px;
+  overflow: hidden;
+  background-color: black;
+  transition: opacity 0.3s, top 0.3s;
+}
+.ea-message_wrap .ea-icon-wrap {
+  margin-right: 0.5rem;
+  line-height: 1;
+}
+.ea-message_wrap .ea-text-content {
+  line-height: 1;
+  margin-right: auto;
+  vertical-align: middle;
+}
+.ea-message_wrap .ea-close-icon {
+  margin-left: auto;
+}
+.ea-message_wrap.ea-message--success {
+  background-color: #f0f9eb;
+  color: #67c23a;
+}
+.ea-message_wrap.ea-message--info {
+  background-color: #f4f4f5;
+  color: #909399;
+}
+.ea-message_wrap.ea-message--warning {
+  background-color: #fdf6ec;
+  color: #e6a23c;
+}
+.ea-message_wrap.ea-message--error {
+  background-color: #fef0f0;
+  color: #f56c6c;
+}
+`;var a,c,g,r;class y extends f{constructor(){super();l(this,a,void 0);l(this,c,void 0);l(this,g,void 0);l(this,r,void 0);const e=this.attachShadow({mode:"open"});e.innerHTML=`
+            <div class="ea-message_wrap" part="container">
+                <ea-icon class="ea-icon-wrap" part="icon"></ea-icon>
+                <div class="ea-text-content" part="content-wrap"></div>
+                <ea-icon class="ea-close-icon" icon="icon-cancel"></ea-icon>
+            </div>
+        `,h(this,a,e.querySelector(".ea-message_wrap")),this.wrap=o(this,a),h(this,c,e.querySelector(".ea-icon-wrap")),h(this,g,e.querySelector(".ea-text-content")),h(this,r,e.querySelector(".ea-close-icon")),this.closeWrap=o(this,r),this.build(e,w)}get attrs(){return["show","text","icon","type","showClose","center"]}get iconList(){return{success:"icon-ok-circled",error:"icon-cancel-circled",warning:"icon-attention-alt",info:"icon-info"}}get show(){return this.getAttrBoolean("show")}set show(e){this.setAttribute("show",e);const n=document.querySelectorAll("ea-message");if(e){const i=n.length-1,d=o(this,a).getBoundingClientRect().height;let p=i<=0?10:(i+1)*10;o(this,a).style.top=`${i*d+p}px`,o(this,a).style.opacity=1}else{o(this,a).style.top="-100%",o(this,a).style.opacity=0;let i=o(this,a).addEventListener("transitionend",()=>{this.removeEventListener("transitionend",i),this.remove()})}}get text(){return this.getAttribute("text")}set text(e){e&&(this.setAttribute("text",e),o(this,g).innerText=e)}get type(){return this.getAttribute("type")||"info"}set type(e){this.setAttribute("type",e),o(this,a).classList.add(`ea-message--${e}`),o(this,c).icon=this.iconList[e]}get showClose(){return this.getAttrBoolean("showClose")||!1}set showClose(e){e&&(this.setAttribute("showClose",e),o(this,r).style.display=e?"block":"none")}get center(){return this.getAttrBoolean("center")||!1}set center(e){e&&(this.setAttribute("center",e),o(this,c).style.marginLeft=e?"auto":"0")}connectedCallback(){o(this,r).addEventListener("click",()=>{this.show=!1})}disconnectedCallback(){const e=document.querySelectorAll("ea-message");e.length>0&&Array.from(e).forEach((i,d)=>{const p=i.wrap.getBoundingClientRect().height;i.wrap.style.top=`${d*p+d*10}px`})}}a=new WeakMap,c=new WeakMap,g=new WeakMap,r=new WeakMap;customElements.get("ea-message")||customElements.define("ea-message",y);window.$message=new u;export{y as EaMessageElement};
---
+++ assets/chunks/index.SnCsTy4-.js
@@ -0,0 +1,92 @@
+var u=(t,a,e)=>{if(!a.has(t))throw TypeError("Cannot "+e)};var r=(t,a,e)=>(u(t,a,"read from private field"),e?e.call(t):a.get(t)),l=(t,a,e)=>{if(a.has(t))throw TypeError("Cannot add the same private member more than once");a instanceof WeakSet?a.add(t):a.set(t,e)},c=(t,a,e,g)=>(u(t,a,"write to private field"),g?g.call(t,e):a.set(t,e),e);var h=(t,a,e)=>(u(t,a,"access private method"),e);import{B as x}from"./Base.LSgLRpFA.js";import"./index.CgEiilRo.js";import{h as y}from"./handleDefaultAttrIsTrue.C-TnTdkH.js";import{c as _}from"./createElement.Dy1aXqlz.js";const k=`
+.ea-alert_wrap {
+  position: relative;
+  box-sizing: border-box;
+  overflow: hidden;
+  border-radius: 4px;
+  padding: 8px 16px;
+  margin: 20px 0 0;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  opacity: 1;
+  transition: opacity 0.2s;
+}
+.ea-alert_wrap .ea-alert_content {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.ea-alert_wrap .ea-alert_content .ea-alert_title {
+  display: flex;
+  align-items: center;
+}
+.ea-alert_wrap .ea-alert_content .ea-alert_title i {
+  margin-right: 0.5rem;
+}
+.ea-alert_wrap .ea-alert_content .ea-alert_close-icon {
+  color: #c0c4cc;
+  cursor: pointer;
+}
+.ea-alert_wrap .ea-alert_content.ea-alert--center .ea-alert_title,
+.ea-alert_wrap .ea-alert_content.ea-alert--center .ea-alert_close-icon {
+  margin-left: auto;
+}
+.ea-alert_wrap .ea-alert_description {
+  width: 100%;
+  margin: 5px 0 0;
+  font-size: 12px;
+}
+.ea-alert_wrap.ea-alert--success {
+  background-color: #f0f9eb;
+  color: #67c23a;
+}
+.ea-alert_wrap.ea-alert--success.ea-alert--dark {
+  color: #fff;
+  background-color: #67c23a;
+}
+.ea-alert_wrap.ea-alert--success.ea-alert--dark .ea-alert_close-icon {
+  color: #fff;
+}
+.ea-alert_wrap.ea-alert--info {
+  background-color: #f4f4f5;
+  color: #909399;
+}
+.ea-alert_wrap.ea-alert--info.ea-alert--dark {
+  color: #fff;
+  background-color: #909399;
+}
+.ea-alert_wrap.ea-alert--info.ea-alert--dark .ea-alert_close-icon {
+  color: #fff;
+}
+.ea-alert_wrap.ea-alert--warning {
+  background-color: #fdf6ec;
+  color: #e6a23c;
+}
+.ea-alert_wrap.ea-alert--warning.ea-alert--dark {
+  color: #fff;
+  background-color: #e6a23c;
+}
+.ea-alert_wrap.ea-alert--warning.ea-alert--dark .ea-alert_close-icon {
+  color: #fff;
+}
+.ea-alert_wrap.ea-alert--error {
+  background-color: #fef0f0;
+  color: #f56c6c;
+}
+.ea-alert_wrap.ea-alert--error.ea-alert--dark {
+  color: #fff;
+  background-color: #f56c6c;
+}
+.ea-alert_wrap.ea-alert--error.ea-alert--dark .ea-alert_close-icon {
+  color: #fff;
+}
+`;var i,n,o,s,p,w,f,b,d,m;class A extends x{constructor(){super();l(this,p);l(this,f);l(this,d);l(this,i,void 0);l(this,n,void 0);l(this,o,void 0);l(this,s,void 0);const e=this.attachShadow({mode:"open"});e.innerHTML=`
+      <div class='ea-alert_wrap' part='container'>
+        <div class="ea-alert_content" part='content-wrap'>
+          <span class="ea-alert_title" part='title'></span>
+          <ea-icon class="ea-alert_close-icon" part='icon'></ea-icon>
+        </div>
+      </div>
+    `,c(this,i,e.querySelector(".ea-alert_wrap")),c(this,n,e.querySelector(".ea-alert_content")),c(this,o,e.querySelector(".ea-alert_title")),c(this,s,e.querySelector(".ea-alert_close-icon")),this.build(e,k)}get type(){return this.getAttribute("type")||"info"}set type(e){this.setAttribute("type",e),r(this,i).classList.add(`ea-alert--${e}`)}get title(){return this.getAttribute("title")||""}set title(e){this.setAttribute("title",e),r(this,o).innerText=e}get closable(){return y("closable")}set closable(e){this.setAttribute("closable",e),r(this,s).style.display=e?"block":"none"}get closeText(){return this.getAttribute("close-text")||""}set closeText(e){this.setAttribute("close-text",e)}get effect(){return this.getAttribute("effect")||"light"}set effect(e){this.setAttribute("effect",e),r(this,i).classList.toggle("ea-alert--dark",e==="dark")}get showIcon(){return this.getAttrBoolean("show-icon")||!1}set showIcon(e){this.setAttribute("show-icon",e)}get center(){return this.getAttrBoolean("center")||!1}set center(e){this.setAttribute("center",e),r(this,n).classList.toggle("ea-alert--center",e)}get description(){return this.getAttribute("description")||""}set description(e){this.setAttribute("description",e)}get iconList(){return{success:"ok-circled",info:"info",warning:"attention-alt",error:"cancel-circled"}}connectedCallback(){this.type=this.type,this.title=this.title,this.closable=this.closable,this.closeText=this.closeText,this.effect=this.effect,this.center=this.center,h(this,p,w).call(this),h(this,f,b).call(this),h(this,d,m).call(this)}}i=new WeakMap,n=new WeakMap,o=new WeakMap,s=new WeakMap,p=new WeakSet,w=function(){this.closable&&(this.closable===!0&&this.closeText===""?r(this,s).icon="icon-cancel":r(this,s).innerText=this.closeText,r(this,s).addEventListener("click",()=>{r(this,i).style.opacity=0,this.dispatchEvent(new CustomEvent("close",{detail:{target:r(this,s)}}))}),r(this,i).addEventListener("transitionend",()=>{this.remove()}))},f=new WeakSet,b=function(){if(!this.showIcon)return;const e=_("ea-icon");e.icon=`icon-${this.iconList[this.type]}`,e.classList.add(`ea-alert--${this.type}`),r(this,o).insertBefore(e,r(this,o).firstChild)},d=new WeakSet,m=function(){if(!this.description)return;const e=_("p","ea-alert_description");e.part="description",r(this,i).style.flexDirection="column",e.innerText=this.description,r(this,i).appendChild(e)};customElements.get("ea-alert")||customElements.define("ea-alert",A);export{A as EaAlert};
