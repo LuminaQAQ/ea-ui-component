e480ece24e9621326f27e43f9ed28452b2b76955
LuminaQAQ <1710884533@qq.com>
2024-09-08 18:58:29 +0800

    docs: EaPagination: 更新文档

--- components/ea-pagination/src/components/getShowTotalItem.js
+++ components/ea-pagination/src/components/getShowTotalItem.js
@@ -1,6 +1,7 @@
 export const getShowTotalItem = () => {
     const showTotalItem = document.createElement('span');
     showTotalItem.className = 'ea-pagination_show_total';
+    showTotalItem.part = 'total-wrap';
 
     return showTotalItem;
 }
