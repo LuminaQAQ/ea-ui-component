cca7e1b4f9cf0a1adf825eb585ab57c9e33fb6c9
LuminaQAQ <1710884533@qq.com>
2024-09-08 17:58:06 +0800

    Updates

--- assets/chunks/index.CQU1UkII.js
+++
@@ -1,7 +0,0 @@
-var c=(e,s,t)=>{if(!s.has(e))throw TypeError("Cannot "+t)};var o=(e,s,t)=>(c(e,s,"read from private field"),t?t.call(e):s.get(e)),r=(e,s,t)=>{if(s.has(e))throw TypeError("Cannot add the same private member more than once");s instanceof WeakSet?s.add(e):s.set(e,t)},l=(e,s,t,n)=>(c(e,s,"write to private field"),n?n.call(e,t):s.set(e,t),t);var i;class h extends HTMLElement{constructor(){super();r(this,i,void 0);const t=this.attachShadow({mode:"open"});t.innerHTML=`
-            <link rel="stylesheet" href="/ea-ui-component/ea-icon/css/animation.css">
-            <link rel="stylesheet" href="/ea-ui-component/ea-icon/css/fontello.css">
-            <i class="ea-icon_wrap" part="container">
-                <slot></slot>
-            </i>
-        `,l(this,i,t.querySelector(".ea-icon_wrap"))}get icon(){return this.getAttribute("icon")||""}set icon(t){this.setAttribute("icon",t),o(this,i).className=`${t}`}get color(){return this.getAttribute("color")||""}set color(t){this.setAttribute("color",t),o(this,i).style.color=t}get size(){return this.getAttribute("size")||""}set size(t){this.setAttribute("size",t),o(this,i).style.fontSize=`${t}px`}connectedCallback(){this.icon=this.icon,this.color=this.color,this.size=this.size}}i=new WeakMap;window.customElements.get("ea-icon")||window.customElements.define("ea-icon",h);export{h as EaIcon};
---
+++ assets/chunks/index.CXLnHdT5.js
@@ -0,0 +1,54 @@
+var o=(t,i,e)=>{if(!i.has(t))throw TypeError("Cannot "+e)};var r=(t,i,e)=>(o(t,i,"read from private field"),e?e.call(t):i.get(t)),l=(t,i,e)=>{if(i.has(t))throw TypeError("Cannot add the same private member more than once");i instanceof WeakSet?i.add(t):i.set(t,e)},c=(t,i,e,s)=>(o(t,i,"write to private field"),s?s.call(t,e):i.set(t,e),e);import{B as a}from"./Base.LSgLRpFA.js";import"./index.ChwP6VyU.js";const d=`
+.ea-link {
+  text-decoration: none;
+  color: #606266;
+  cursor: pointer;
+}
+.ea-link:hover {
+  color: #797b80;
+}
+.ea-link.underline:hover {
+  text-decoration: underline;
+}
+.ea-link.primary {
+  color: #409eff;
+}
+.ea-link.primary:hover {
+  color: #73b8ff;
+}
+.ea-link.success {
+  color: #67c23a;
+}
+.ea-link.success:hover {
+  color: #85cf60;
+}
+.ea-link.info {
+  color: #909399;
+}
+.ea-link.info:hover {
+  color: #abadb1;
+}
+.ea-link.warning {
+  color: #e6a23c;
+}
+.ea-link.warning:hover {
+  color: #ecb869;
+}
+.ea-link.danger {
+  color: #f56c6c;
+}
+.ea-link.danger:hover {
+  color: #f89c9c;
+}
+.ea-link.disabled {
+  color: #c0c4cc;
+  pointer-events: none;
+}
+.ea-link.disabled:hover {
+  color: #dcdee3;
+}
+`;var n;class h extends a{constructor(){super();l(this,n,void 0);const e=this.attachShadow({mode:"open"});e.innerHTML=`
+      <a class="ea-link" part="container">
+        <slot></slot>
+      </a>
+    `,c(this,n,e.querySelector(".ea-link")),this.build(e,d)}get LINK_TYPE(){return["primary","success","info","warning","danger"]}get href(){return this.getAttribute("href")}set href(e){e&&(this.setAttribute("href",e),r(this,n).href=e)}get type(){const e=this.getAttribute("type");return this.LINK_TYPE.includes(e)?e:null}set type(e){e&&this.LINK_TYPE.includes(e)&&r(this,n).classList.add(e)}get disabled(){return this.getAttrBoolean("disabled")}set disabled(e){this.setAttribute("disabled",e),r(this,n).classList.toggle("disabled",e),this.style.cursor=e?"not-allowed":"pointer"}get underline(){return this.getAttrBoolean("underline")}set underline(e){this.setAttribute("underline",e),r(this,n).classList.toggle("underline",e)}get icon(){return this.getAttribute("icon")}set icon(e){if(!e)return;const s=document.createElement("ea-icon");s.icon=e,r(this,n).insertBefore(s,r(this,n).firstChild)}connectedCallback(){this.style.display="inline-block",this.href=this.href,this.type=this.type,this.disabled=this.disabled,this.underline=this.underline,this.icon=this.icon}}n=new WeakMap;window.customElements.get("ea-link")||window.customElements.define("ea-link",h);export{h as EaLink};
