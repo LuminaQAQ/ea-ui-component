5d69799640920b3d0e1f08c290342d2c72f8ef9b
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-07-22 23:04:13 +0800

    feat(ea-scrollbar): 完成功能，未写属性和文档

--- src/components/ea-scrollbar/index.js
+++ src/components/ea-scrollbar/index.js
@@ -6,9 +6,13 @@ export class EaScrollbar extends Base {
     /** @type {HTMLElement} */
     #container;
     /** @type {HTMLElement} */
-    #track;
+    #horizontalTrack;
     /** @type {HTMLElement} */
-    #thumb;
+    #verticalTrack;
+    /** @type {HTMLElement} */
+    #verticalThumb;
+    /** @type {HTMLElement} */
+    #horizontalThumb;
     /** @type {HTMLElement} */
     #view
 
@@ -50,8 +54,11 @@ export class EaScrollbar extends Base {
     $render() {
         this.shadowRoot.innerHTML = `
             <div class="ea-scrollbar" part="container">
-                <div class="ea-scrollbar__track" part="track">
-                    <div class="ea-scrollbar__thumb" part="thumb"></div>
+                <div class="ea-scrollbar__track ea-scrollbar__track-horizontal" part="track-horizontal">
+                    <div class="ea-scrollbar__thumb-horizontal" part="thumb"></div>
+                </div>
+                <div class="ea-scrollbar__track ea-scrollbar__track-vertical" part="track-vertical">
+                    <div class="ea-scrollbar__thumb-vertical" part="thumb"></div>
                 </div>
                 <div class="ea-scrollbar__view" part="view-container">
                     <slot></slot>
@@ -60,35 +67,90 @@ export class EaScrollbar extends Base {
         `;
 
         this.#container = this.shadowRoot.querySelector('.ea-scrollbar');
-        this.#track = this.shadowRoot.querySelector('.ea-scrollbar__track');
-        this.#thumb = this.shadowRoot.querySelector('.ea-scrollbar__thumb');
+        this.#verticalTrack = this.shadowRoot.querySelector('.ea-scrollbar__track-vertical');
+        this.#horizontalTrack = this.shadowRoot.querySelector('.ea-scrollbar__track-horizontal');
+        this.#verticalThumb = this.shadowRoot.querySelector('.ea-scrollbar__thumb-vertical');
+        this.#horizontalThumb = this.shadowRoot.querySelector('.ea-scrollbar__thumb-horizontal');
         this.#view = this.shadowRoot.querySelector('.ea-scrollbar__view');
     }
 
     #scrollEvent = () => {
-        this.#thumb.style.setProperty('--ea-scrollbar-top', `${this.#view.scrollTop / this.#view.scrollHeight * 100}%`);
+        this.#verticalThumb.style.setProperty('--ea-scrollbar-top', `${this.#view.scrollTop / this.#view.scrollHeight * 100}%`);
+        this.#horizontalThumb.style.setProperty('--ea-scrollbar-left', `${this.#view.scrollLeft / this.#view.scrollWidth * 100}%`);
     };
 
     #resizeEvent = () => {
-        const thumbHeight = this.#view.clientHeight / this.#view.scrollHeight;
-        this.#thumb.style.setProperty('--ea-scrollbar-thumb-height', `${thumbHeight * 100}%`);
-        this.#track.classList.toggle('is-show', thumbHeight >= 1);
+        const verticalThumbHeight = this.#view.clientHeight / this.#view.scrollHeight;
+        const horizontalThumbWidth = this.#view.clientWidth / this.#view.scrollWidth;
+
+        this.#verticalThumb.style.setProperty('--ea-scrollbar-thumb-vertical-height', `${verticalThumbHeight * 100}%`);
+        this.#horizontalThumb.style.setProperty('--ea-scrollbar-thumb-horizontal-width', `${horizontalThumbWidth * 100}%`);
+
+        this.#verticalTrack.classList.toggle('is-show', verticalThumbHeight >= 1);
+        this.#horizontalTrack.classList.toggle('is-show', horizontalThumbWidth >= 1);
+    }
+
+    #verticalMouseMoveEvent = (e) => {
+        const thumbHeight = this.#verticalThumb.offsetHeight / 2;
+        const initTop = e.clientY - thumbHeight + (this.#view.scrollTop / this.#view.scrollHeight)
+
+        this.#view.scrollTo({
+            top: initTop * this.#view.scrollHeight / this.#view.clientHeight,
+            behavior: "instant"
+        });
+
+        this.#verticalThumb.classList.add('is-active');
+    }
+
+    #horizontalMouseMoveEvent = (e) => {
+        const thumbWidth = this.#horizontalThumb.offsetWidth / 2;
+        const initLeft = e.clientX - thumbWidth + (this.#view.scrollLeft / this.#view.scrollWidth)
+
+        this.#view.scrollTo({
+            left: initLeft * this.#view.scrollWidth / this.#view.clientWidth,
+            behavior: "instant"
+        });
+
+        this.#horizontalThumb.classList.add('is-active');
+    }
+
+    /**
+     * 
+     * @param {MouseEvent} e 
+     */
+    #mouseDownEvent = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const controller = new AbortController();
+
+        const isHorizontal = e.target === this.#horizontalThumb;
+
+        window.addEventListener('mousemove', isHorizontal ? this.#horizontalMouseMoveEvent : this.#verticalMouseMoveEvent, { signal: controller.signal });
+
+        window.addEventListener('mouseup', () => {
+            controller.abort();
+            this.#horizontalThumb.classList.remove('is-active');
+            this.#verticalThumb.classList.remove('is-active');
+        }, { signal: controller.signal });
     }
 
     connectedCallback() {
         super.connectedCallback();
 
-        this.evventController = new AbortController();
-        const controller = this.evventController;
+        this.eventController = new AbortController();
+        const controller = this.eventController;
 
         this.#view.addEventListener('scroll', this.#scrollEvent, { signal: controller.signal });
-        window.addEventListener('resize', this.#resizeEvent, { signal: controller.signal });
+        this.#horizontalThumb.addEventListener('mousedown', this.#mouseDownEvent, { signal: controller.signal });
+        this.#verticalThumb.addEventListener('mousedown', this.#mouseDownEvent, { signal: controller.signal });
 
+        window.addEventListener('resize', this.#resizeEvent, { signal: controller.signal });
         window.addEventListener('load', this.#resizeEvent, { signal: controller.signal })
     }
 
     $beforeUnmounted() {
-        this.evventController?.abort();
+        this.eventController?.abort();
     }
 }
 if (!window.customElements.get('ea-scrollbar')) {
