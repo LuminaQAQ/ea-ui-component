230e97917ab8abfbd048c6bbb4b83ad5fa1248d0
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-08-24 00:00:23 +0800

    feat(ea-popper): 增加flip属性

--- src/common/ea-popper/index.js
+++ src/common/ea-popper/index.js
@@ -2,6 +2,54 @@ import Base from '@components/Base.js'
 
 import stylesheet from './index.scss?inline';
 
+/**
+ * 检查视口可见
+ * @param {HTMLElement} el 
+ * @returns 
+ */
+const isIntersecting = (el) => {
+    const rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= window.innerHeight &&
+        rect.right <= window.innerWidth
+    );
+}
+
+/**
+ * 根据视口情况翻转 placement
+ * @param {HTMLElement} el 
+ * @param {string} placement
+ * @returns {string}
+ */
+const flipPlacement = (el, placement) => {
+    const antiPlacement = {
+        left: 'right',
+        right: 'left',
+        top: 'bottom',
+        bottom: 'top',
+    }
+
+    const rect = el.getBoundingClientRect();
+    const strategies = {
+        top: rect.top < 0 && placement.includes("top"),
+        bottom: rect.bottom > window.innerHeight && placement.includes("bottom"),
+        left: rect.left < 0 && placement.includes("left"),
+        right: rect.right > window.innerWidth && placement.includes("right"),
+    }
+
+
+    if (isIntersecting(el)) return placement;
+
+    for (const strategy in strategies) {
+        if (strategies[strategy]) return placement.replace(strategy, antiPlacement[strategy]);
+    }
+
+    return placement;
+}
+
 export class EaPopper extends Base {
     /** @type {HTMLElement} */
     #container;
@@ -13,10 +61,7 @@ export class EaPopper extends Base {
     #statusAbortController;
 
     static get observedAttributes() {
-        return [
-            'placement', 'show-arrow', 'status', 'offset',
-            // 'filp'
-        ];
+        return ['placement', 'show-arrow', 'status', 'offset', 'filp'];
     }
 
     state = this.properties({
@@ -45,7 +90,10 @@ export class EaPopper extends Base {
                     this.#container.className = this.updateContainerClasslist();
                     this.#dispatchBubblesEvent('show');
 
-                    this.#container.offsetWidth;
+                    if (this.flip) this.placement = flipPlacement(this.#originalPopper, this.placement);;
+
+                    void this.#container.offsetWidth;
+
                     this.#container.classList.add('ea-popper--is-show');
 
                     this.#container.addEventListener('transitionend', () => {
@@ -85,13 +133,11 @@ export class EaPopper extends Base {
                 }
             }
         },
-        // flip: {
-        //     type: Boolean,
-        //     default: false,
-        //     observer: (newVal) => {
-        //         this.#container.className = this.updateContainerClasslist();
-        //     }
-        // },
+        flip: {
+            type: Boolean,
+            default: false,
+            observer: (newVal) => { }
+        },
     })
 
     /**
@@ -103,7 +149,6 @@ export class EaPopper extends Base {
             ['--' + this.placement]: this.placement,
             ['--show-arrow']: this['show-arrow'],
             ['--show']: this.status,
-            // ['--flip']: this.status && this.flip,
         });
     }
 
@@ -156,26 +201,6 @@ export class EaPopper extends Base {
         super.connectedCallback();
 
         this.placement = this.placement;
-
-        // const antiPlacement = {
-        //     left: 'right',
-        //     right: 'left',
-        //     top: 'bottom',
-        //     bottom: 'top',
-        // }
-
-        // let isFlip = false;
-        // const intersectionObserver = new IntersectionObserver((entries) => {
-        //     entries.forEach((entry) => {
-        //         if (!entry.isIntersecting && this.status && !isFlip) {
-        //             const placement = Object.keys(antiPlacement).find(key => this.placement.includes(key));
-
-        //             this.placement = this.placement?.replace(placement, antiPlacement[placement]);
-        //             isFlip = true;
-        //         }
-        //     });
-        // });
-        // intersectionObserver.observe(this.#originalPopper);
     }
 }
 
