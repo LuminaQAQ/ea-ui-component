b610c60ae351f49f70604e2368527c695ec82b87
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-09-01 16:34:20 +0800

    temp（ea-message）

--- src/components/ea-message/index.js
+++ src/components/ea-message/index.js
@@ -1,171 +1,136 @@
-// @ts-nocheck
-import Base from '../Base.js';
-import { createElement } from '../../utils/createElement.js';
-import "../ea-icon/index.js"
+import Base from '@components/Base.js'
+import EaMessageInstance from './utils/EaMessageInstance';
 
-import { EaMessage } from "./src/utils/MessageClass.js"
+import stylesheet from './index.scss?inline';
 
-import { stylesheet } from './src/style/stylesheet.js';
+export class EaMessage extends Base {
+    /** @type {HTMLElement} */
+    #container;
+    /** @type {HTMLElement} */
+    #messageIcon;
+    /** @type {HTMLElement} */
+    #messageContent;
+    /** @type {HTMLElement} */
+    #messageCloseIcon;
+    /** @type {AbortController} */
+    #visibleAbortController;
 
-export class EaMessageElement extends Base {
-    #wrap;
+    static get observedAttributes() {
+        return ['type', 'visible', 'message', 'showClose'];
+    }
 
-    #icon;
+    state = this.properties({
+        type: {
+            type: ['primary', 'success', 'warning', 'info', 'error'],
+            default: 'info',
+            observer: (newVal) => {
+                const iconTypes = {
+                    "success": "icon-ok-circled",
+                    "error": "icon-cancel-circled",
+                    "warning": "icon-attention-alt",
+                    "info": "icon-info",
+                    "primary": "icon-info",
+                };
 
-    #textContent;
+                this.#messageIcon.icon = iconTypes[newVal];
+                this.#container.className = this.updateContainerClasslist();
+            }
+        },
+        visible: {
+            type: Boolean,
+            default: false,
+            observer: (newVal) => {
+                this.#visibleAbortController?.abort();
+                this.#visibleAbortController = new AbortController();
 
-    #closeWrap;
+                if (newVal) {
+                    this.#container.className = this.updateContainerClasslist();
+                    this.#dispatchBubblesEvent('show');
+
+                    void this.#container.offsetWidth;
+
+                    this.#container.classList.add('ea-message--is-show');
+
+                    this.#container.addEventListener('transitionend', () => {
+                        this.#dispatchBubblesEvent('shown');
+                    }, { once: true, signal: this.#visibleAbortController.signal })
+                } else {
+                    this.#container.classList.add('ea-message--before-hide');
+                    this.#dispatchBubblesEvent('hide');
+
+                    this.#container.addEventListener('transitionend', () => {
+                        this.#container.className = this.updateContainerClasslist();
+                        this.#dispatchBubblesEvent('hidden');
+                    }, { once: true, signal: this.#visibleAbortController.signal })
+                }
+            }
+        },
+        message: {
+            type: String,
+            default: '',
+            observer: (newVal) => {
+                this.#messageContent.innerText = newVal;
+            }
+        },
+        showClose: {
+            type: Boolean,
+            default: false,
+            observer: (newVal) => {
+                this.#container.className = this.updateContainerClasslist();
+            }
+        },
+    })
+
+    /**
+     * 获取 classlist 列表
+     * @return {string} 属性值
+     */
+    updateContainerClasslist() {
+        return this.computedClasslist('ea-message', {
+            ['--visible']: this.visible,
+            ['--' + this.type]: this.type,
+            ['--show-close']: this.showClose,
+        });
+    }
 
     constructor() {
         super();
 
-        const shadowRoot = this.attachShadow({ mode: 'open' });
-        shadowRoot.innerHTML = `
-            <div class="ea-message_wrap" part="container">
-                <ea-icon class="ea-icon-wrap" part="icon"></ea-icon>
-                <div class="ea-text-content" part="content-wrap"></div>
-                <ea-icon class="ea-close-icon" icon="icon-cancel"></ea-icon>
+        this.stylesheet = stylesheet;
+
+        this.$render();
+    }
+
+    $render() {
+        this.shadowRoot.innerHTML = `
+            <div class="ea-message" part="container">
+                <ea-icon class="ea-message__icon" part="icon"></ea-icon>
+                <div class="ea-message__content" part="content-wrap"></div>
+                <ea-icon class="ea-message__icon-close" icon="icon-cancel"></ea-icon>
             </div>
         `;
 
-        this.#wrap = shadowRoot.querySelector('.ea-message_wrap');
-        this.wrap = this.#wrap;
-        this.#icon = shadowRoot.querySelector('.ea-icon-wrap');
-        this.#textContent = shadowRoot.querySelector('.ea-text-content');
-        this.#closeWrap = shadowRoot.querySelector('.ea-close-icon');
-        this.closeWrap = this.#closeWrap;
-
-        this.build(shadowRoot, stylesheet);
+        this.#container = this.shadowRoot.querySelector('.ea-message');
+        this.#messageIcon = this.shadowRoot.querySelector('.ea-message__icon');
+        this.#messageContent = this.shadowRoot.querySelector('.ea-message__content');
+        this.#messageCloseIcon = this.shadowRoot.querySelector('.ea-message__icon-close');
     }
 
-    get attrs() {
-        return ["show", "text", "icon", "type", "showClose", "center"];
+    #dispatchBubblesEvent = (customEventName, detail) => {
+        this.dispatchEvent(new CustomEvent(customEventName, {
+            detail,
+            bubbles: true,
+            composed: true,
+        }));
     }
-
-    get iconList() {
-        return {
-            "success": "icon-ok-circled",
-            "error": "icon-cancel-circled",
-            "warning": "icon-attention-alt",
-            "info": "icon-info"
-        };
-    }
-
-    // ------- show 提示框的显示状态 -------
-    // #region
-    get show() {
-        return this.getAttrBoolean('show');
-    }
-
-    set show(value) {
-        this.setAttribute('show', value);
-
-        const eaMessageList = document.querySelectorAll('ea-message');
-
-        if (value) {
-            const length = eaMessageList.length - 1;
-            const elementHeight = this.#wrap.getBoundingClientRect().height;
-            let gap = length <= 0 ? 10 : (length + 1) * 10;
-
-            this.#wrap.style.top = `${length * elementHeight + gap}px`;
-            this.#wrap.style.opacity = 1;
-        } else {
-            this.#wrap.style.top = `-100%`;
-            this.#wrap.style.opacity = 0;
-
-            let event = this.#wrap.addEventListener('transitionend', () => {
-                this.removeEventListener('transitionend', event);
-                this.remove();
-            });
-        }
-
-    }
-    // #endregion
-    // ------- end -------
-
-    // ------- text 提示框的文字 -------
-    // #region
-    get text() {
-        return this.getAttribute('text');
-    }
-
-    set text(value) {
-        if (!value) return;
-        this.setAttribute('text', value);
-
-        this.#textContent.innerText = value;
-    }
-    // #endregion
-    // ------- end -------
-
-    // ------- type 提示框的类型 -------
-    // #region
-    get type() {
-        return this.getAttribute('type') || "info";
-    }
-
-    set type(value) {
-        this.setAttribute('type', value);
-
-        this.#wrap.classList.add(`ea-message--${value}`);
-        this.#icon.icon = this.iconList[value];
-    }
-    // #endregion
-    // ------- end -------
-
-    // ------- showClose 提示框的关闭按钮 -------
-    // #region
-    get showClose() {
-        return this.getAttrBoolean('showClose') || false;
-    }
-
-    set showClose(value) {
-        if (!value) return;
-
-        this.setAttribute('showClose', value);
-
-        this.#closeWrap.style.display = value ? "block" : "none";
-    }
-    // #endregion
-    // ------- end -------
-
-    // ------- center 提示框是否居中 -------
-    // #region
-    get center() {
-        return this.getAttrBoolean('center') || false;
-    }
-
-    set center(value) {
-        if (!value) return;
-
-        this.setAttribute('center', value);
-
-        this.#icon.style.marginLeft = value ? `auto` : `0`;
-    }
-    // #endregion
-    // ------- end -------
 
     connectedCallback() {
-        this.#closeWrap.addEventListener('click', () => {
-            this.show = false;
-        });
-    }
-
-    disconnectedCallback() {
-        const eaMessageList = document.querySelectorAll('ea-message');
-        const length = eaMessageList.length;
-
-        if (length > 0) {
-            Array.from(eaMessageList).forEach((el, index) => {
-                const elementHeight = el.wrap.getBoundingClientRect().height;
-                el.wrap.style.top = `${index * elementHeight + (index * 10)}px`;
-            });
-        }
+        super.connectedCallback();
     }
 }
 
-if (!customElements.get('ea-message')) {
-    customElements.define('ea-message', EaMessageElement);
+if (!window.customElements.get('ea-message')) {
+    window.customElements.define('ea-message', EaMessage);
 }
 
-window.$message = new EaMessage();
+window.$message = EaMessageInstance
---
+++ src/components/ea-message/utils/EaMessageInstance.js
@@ -0,0 +1,67 @@
+/**
+ * @typedef {Object} EaMessageOptions
+ * @property {string} message - 消息文字
+ * @property {'top' | 'top-left' | 'top-right' | 'bottom' | 'bottom-left' | 'bottom-right'} placement - 消息出现的位置，可选值为 'top' | 'top-left' | 'top-right' | 'bottom' | 'bottom-left' | 'bottom-right'
+ * @property {'primary' | 'success' | 'warning' | 'info' | 'error'} type - 主题类型，可选值为 'primary' | 'success' | 'warning' | 'info' | 'error'
+ * @property {Boolean} dangerouslyUseHTMLString - 是否将 message 属性作为 HTML 片段处理
+ * @property {Boolean} show-close - 是否显示关闭按钮
+ * @property {Number} duration - 显示时间，毫秒。设为 0 则不会自动关闭
+ * @property {Function} onClose - 	关闭时的回调函数, 参数为被关闭的 message 实例
+ * @property {Boolean} offset - 设置到视口边缘的距离（当位置为'top'时为顶部，当位置为'bottom'时为底部）
+ * @property {Boolean} appendTo - 设置 message 的根元素，默认为 `document.body`
+ */
+
+import { timeout } from "@/utils/timeout";
+
+const includeTypes = ['message', 'placement', 'type', 'showClose', 'duration', 'offset'];
+
+const appendToHandler = (el, appendTo) => {
+    if (appendTo instanceof HTMLElement) {
+        appendTo.appendChild(el);
+    } else {
+        const appendTo = document.querySelector(appendTo);
+        appendTo ? appendTo.appendChild(el) : document.body.appendChild(el);
+    }
+}
+const renderer = (options) => {
+    const el = document.createElement('ea-message');
+
+    if (options instanceof String) {
+        el.message = options;
+    } else if (options instanceof Object) {
+        for (const option in options) {
+            el[option] = options[option];
+        }
+    }
+
+    return el;
+}
+
+/**
+ * @param {EaMessageOptions} options
+ */
+const EaMessageInstance = (options) => {
+    const messageOptions = includeTypes.reduce((acc, cur) => {
+        if (options[cur]) acc[cur] = options[cur];
+
+        return acc;
+    }, {});
+    const el = renderer(messageOptions);
+
+    if (options.appendTo instanceof HTMLElement) {
+        options.appendTo.appendChild(el);
+    } else {
+        const appendTo = document.querySelector(options.appendTo);
+        appendTo ? appendTo.appendChild(el) : document.body.appendChild(el);
+    }
+
+    el.visible = true;
+    timeout(() => {
+        el.visible = false;
+    }, options.duration || 3000)
+    el.addEventListener('hidden', () => {
+        el.remove();
+    }, { once: true })
+}
+
+export default EaMessageInstance;
