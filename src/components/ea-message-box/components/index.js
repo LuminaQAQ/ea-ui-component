a9f05a32b3515289b8a77a3dfb4484740780e705
LuminaQAQ <1710884533@qq.com>
2025-09-05 07:46:48 +0800

    temp(ea-message-box)

--- src/components/ea-message-box/components/index.js
+++ src/components/ea-message-box/components/index.js
@@ -1,143 +1,199 @@
-import { EaOverlay } from '@/common/ea-overlay';
-// import { EaMessageAlertBox } from "./ea-message-alert-box/index.js"
+import { EaOverlay } from "@/common/ea-overlay";
 
-import stylesheet from './index.scss?inline';
+import stylesheet from "./index.scss?inline";
 
 export class EaMessageBoxElement extends EaOverlay {
-    /** @type {HTMLElement} */
-    #container;
-    /** @type {HTMLElement} */
-    #header;
-    /** @type {HTMLElement} */
-    #title;
-    /** @type {HTMLElement} */
-    #closeIcon;
-    /** @type {HTMLElement} */
-    #content;
-    /** @type {HTMLElement} */
-    #footer;
-    /** @type {HTMLElement} */
-    #confirmButton;
-    /** @type {AbortController} */
-    #abortController;
+  /** @type {HTMLElement} */
+  #container;
+  /** @type {HTMLElement} */
+  #header;
+  /** @type {HTMLElement} */
+  #title;
+  /** @type {HTMLElement} */
+  #typeIcon;
+  /** @type {HTMLElement} */
+  #closeIcon;
+  /** @type {HTMLElement} */
+  #content;
+  /** @type {HTMLElement} */
+  #footer;
+  /** @type {HTMLElement} */
+  #confirmButton;
+  /** @type {HTMLElement} */
+  #cancelButton;
+  /** @type {AbortController} */
+  #abortController;
 
-    static get observedAttributes() {
-        return [
-            ...super.observedAttributes,
-            'boxType',
-            'visible',
-            'dangerouslyUseHTMLString', 'title', 'message',
-            'type', 'icon', 'showClose', 'closeIcon',
-            'cancelButtonText', 'confirmButtonText',
-        ]
-            .map(s => s.replace(/([a-z0-9])([A-Z])/g, "$1-$2")
-                .replace(/([A-Z]+)([A-Z][a-z])/g, "$1-$2")
-                .toLowerCase());
-    }
+  static get observedAttributes() {
+    return [
+      ...super.observedAttributes,
+      "boxType",
+      "visible",
+      "dangerouslyUseHTMLString",
+      "title",
+      "message",
+      "type",
+      "icon",
+      "showClose",
+      "closeIcon",
+      "showCancelButton",
+      "showConfirmButton",
+      "cancelButtonText",
+      "confirmButtonText",
+      "closeOnClickModal",
+      "center",
+    ].map((s) =>
+      s
+        .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
+        .replace(/([A-Z]+)([A-Z][a-z])/g, "$1-$2")
+        .toLowerCase()
+    );
+  }
 
-    state = this.properties({
-        boxType: {
-            type: ['alert' | 'confirm' | 'prompt'],
-            default: 'alert',
-            observer: async (newVal) => {
-                const contentContainer = this.shadowRoot.querySelector('.ea-overlay__content');
-                this.#initVariant[this['box-type']](contentContainer);
-            }
-        },
-        visible: {
-            type: Boolean,
-            default: false,
-            observer: async (newVal) => {
-                this.status = newVal;
-            }
-        },
-        dangerouslyUseHTMLString: {
-            type: Boolean,
-            default: false,
-            observer: (newVal) => { }
-        },
-        title: {
-            type: String,
-            default: '',
-            observer: (newVal) => {
-                this.#title.textContent = newVal;
-            }
-        },
-        message: {
-            type: String,
-            default: '',
-            observer: (newVal) => {
-                if (this.dangerouslyUseHTMLString) {
-                    this.#content.innerHTML = newVal;
-                } else {
-                    this.#content.textContent = newVal;
-                }
-            }
-        },
-        type: {
-            type: ['normal', 'primary', 'success', 'warning', 'danger'],
-            default: 'primary',
-            observer: (newVal) => {
-                this.#confirmButton.type = newVal;
-            }
-        },
-        icon: {
-            type: String,
-            default: '',
-            observer: (newVal) => {
-            }
-        },
-        closeIcon: {
-            type: String,
-            default: 'icon-cancel',
-            observer: (newVal) => {
-                this.#closeIcon.icon = newVal;
-            }
-        },
-        showClose: {
-            type: Boolean,
-            default: true,
-            observer: (newVal) => {
-                this.#closeIcon.style.display = newVal ? 'block' : 'none';
-            }
-        },
-        confirmButtonText: {
-            type: String,
-            default: 'OK',
-            observer: (newVal) => {
-                this.#confirmButton.textContent = newVal;
-            }
-        },
-    })
+  state = this.properties({
+    boxType: {
+      type: ["alert" | "confirm" | "prompt"],
+      default: "alert",
+      observer: async (newVal) => {
+        const contentContainer = this.shadowRoot.querySelector(
+          ".ea-overlay__content"
+        );
+        this.#initVariant[this["box-type"]](contentContainer);
+      },
+    },
+    visible: {
+      type: Boolean,
+      default: false,
+      observer: async (newVal) => {
+        this.status = newVal;
+        this.#container.className = this.updateContainerClasslist();
+      },
+    },
+    dangerouslyUseHTMLString: {
+      type: Boolean,
+      default: false,
+      observer: (newVal) => {},
+    },
+    title: {
+      type: String,
+      default: "",
+      observer: (newVal) => {
+        this.#title.textContent = newVal;
+      },
+    },
+    message: {
+      type: String,
+      default: "",
+      observer: (newVal) => {
+        if (this.dangerouslyUseHTMLString) {
+          this.#content.innerHTML = newVal;
+        } else {
+          this.#content.textContent = newVal;
+        }
+      },
+    },
+    icon: {
+      type: String,
+      default: "",
+      observer: (newVal) => {
+        this.#typeIcon.icon = newVal;
+      },
+    },
+    type: {
+      type: ["primary", "success", "info", "warning", "error"],
+      default: "",
+      observer: (newVal) => {
+        const iconType = {
+          primary: "info",
+          success: "ok-circled",
+          info: "info",
+          warning: "attention-alt",
+          error: "cancel-circled",
+        };
 
-    /**
-     * 获取 classlist 列表
-     * @return {string} 属性值
-     */
-    updateContainerClasslist() {
-        return `${super.updateContainerClasslist()} ${this.computedClasslist('ea-message-box', {
-            ['--visible']: this.visible,
-        })}`;
-    }
+        if (!this.icon && newVal)
+          this.#typeIcon.icon = `icon-${iconType[newVal]}`;
+      },
+    },
+    closeIcon: {
+      type: String,
+      default: "icon-cancel",
+      observer: (newVal) => {
+        this.#closeIcon.icon = newVal;
+      },
+    },
+    showClose: {
+      type: Boolean,
+      default: true,
+      observer: (newVal) => {
+        this.#closeIcon.style.display = newVal ? "block" : "none";
+      },
+    },
+    showCancelButton: {
+      type: Boolean,
+      default: false,
+      observer: (newVal) => {
+        if (this.#cancelButton)
+          this.#cancelButton.style.display = newVal ? "inline-block" : "none";
+      },
+    },
+    showConfirmButton: {
+      type: Boolean,
+      default: true,
+      observer: (newVal) => {
+        if (this.#confirmButton)
+          this.#confirmButton.style.display = newVal ? "inline-block" : "none";
+      },
+    },
+    confirmButtonText: {
+      type: String,
+      default: "OK",
+      observer: (newVal) => {
+        this.#confirmButton.textContent = newVal;
+      },
+    },
+    center: {
+      type: Boolean,
+      default: false,
+      observer: (newVal) => {
+        this.#container.className = this.updateContainerClasslist();
+      },
+    },
+  });
 
-    constructor() {
-        super();
-    }
+  /**
+   * 获取 classlist 列表
+   * @return {string} 属性值
+   */
+  updateContainerClasslist() {
+    return `${super.updateContainerClasslist()} ${this.computedClasslist(
+      "ea-message-box",
+      {
+        ["--visible"]: this.visible,
+        ["--center"]: this.center,
+      }
+    )}`;
+  }
 
-    #dispatchBubblesEvent = (customEventName, detail) => {
-        this.dispatchEvent(new CustomEvent(customEventName, {
-            detail,
-            bubbles: true,
-            composed: true,
-        }));
-    }
+  #dispatchBubblesEvent = (customEventName, detail) => {
+    this.dispatchEvent(
+      new CustomEvent(customEventName, {
+        detail,
+        bubbles: true,
+        composed: true,
+      })
+    );
+  };
 
-    #initVariant = {
-        alert: (container) => {
-            container.innerHTML = `
+  #initVariant = {
+    alert: (container) => {
+      container.innerHTML = `
                 <div class='ea-message-alert-box' part='container'>
                     <header class="ea-message-alert-box__header" part="header">
-                        <span class="ea-message-alert-box__title" part="title"></span>
+                        <div class="ea-message-confirm-box__title-container">
+                            <ea-icon class="ea-message-confirm-box__type-icon" part="type-icon"></ea-icon>
+                            <span class="ea-message-confirm-box__title" part="title"></span>
+                        </div>
                         <ea-icon class="ea-message-alert-box__icon-close" icon="icon-cancel" part='close-icon'></ea-icon>
                     </header>
                     <main class="ea-message-alert-box__content" part="content"></main>
@@ -147,49 +203,126 @@ export class EaMessageBoxElement extends EaOverlay {
                 </div>
             `;
 
-            this.#container = this.shadowRoot.querySelector('.ea-message-alert-box');
-            this.#header = this.shadowRoot.querySelector('.ea-message-alert-box__header');
-            this.#title = this.shadowRoot.querySelector('.ea-message-alert-box__title');
-            this.#closeIcon = this.shadowRoot.querySelector('.ea-message-alert-box__icon-close');
-            this.#content = this.shadowRoot.querySelector('.ea-message-alert-box__content');
-            this.#footer = this.shadowRoot.querySelector('.ea-message-alert-box__footer');
-            this.#confirmButton = this.shadowRoot.querySelector('.ea-message-alert-box__button');
+      this.#container = this.shadowRoot.querySelector(".ea-overlay");
+      this.#header = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__header"
+      );
+      this.#typeIcon = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__type-icon"
+      );
+      this.#title = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__title"
+      );
+      this.#closeIcon = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__icon-close"
+      );
+      this.#content = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__content"
+      );
+      this.#footer = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__footer"
+      );
+      this.#confirmButton = this.shadowRoot.querySelector(
+        ".ea-message-alert-box__button"
+      );
+    },
+    confirm: (container) => {
+      container.innerHTML = `
+                <div class='ea-message-confirm-box' part='container'>
+                    <header class="ea-message-confirm-box__header" part="header">
+                        <div class="ea-message-confirm-box__title-container">
+                            <ea-icon class="ea-message-confirm-box__type-icon" part="type-icon"></ea-icon>
+                            <span class="ea-message-confirm-box__title" part="title"></span>
+                        </div>
+                        <ea-icon class="ea-message-confirm-box__icon-close" icon="icon-cancel" part='close-icon'></ea-icon>
+                    </header>
+                    <main class="ea-message-confirm-box__content" part="content"></main>
+                    <footer class="ea-message-confirm-box__footer" part="footer">
+                        <ea-button class="ea-message-confirm-box__cancel-button">Cancel</ea-button>
+                        <ea-button class="ea-message-confirm-box__confirm-button" type="primary">OK</ea-button>
+                    </footer>
+                </div>
+            `;
+
+      this.#container = this.shadowRoot.querySelector(".ea-overlay");
+      this.#header = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__header"
+      );
+      this.#title = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__title"
+      );
+      this.#typeIcon = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__type-icon"
+      );
+      this.#closeIcon = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__icon-close"
+      );
+      this.#content = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__content"
+      );
+      this.#footer = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__footer"
+      );
+      this.#cancelButton = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__cancel-button"
+      );
+      this.#confirmButton = this.shadowRoot.querySelector(
+        ".ea-message-confirm-box__confirm-button"
+      );
+    },
+  };
+
+  connectedCallback() {
+    this.setAttribute("role", "dialog");
+    this["content-width"] = "100%";
+    this["content-max-width"] = "420px";
+    this["content-height"] = "auto";
+
+    super.connectedCallback();
+
+    this.assignedStyle(stylesheet);
+
+    this.#abortController = new AbortController();
+
+    this.#confirmButton.addEventListener(
+      "click",
+      () => {
+        this.#dispatchBubblesEvent("confirm");
+      },
+      { once: true, signal: this.#abortController.signal }
+    );
+
+    this.#closeIcon.addEventListener(
+      "click",
+      () => {
+        this.#dispatchBubblesEvent("cancel");
+      },
+      { once: true, signal: this.#abortController.signal }
+    );
+
+    if (this.#cancelButton)
+      this.#cancelButton.addEventListener(
+        "click",
+        () => {
+          this.#dispatchBubblesEvent("cancel");
         },
-        confirm: () => {
-        },
-    }
+        { once: true, signal: this.#abortController.signal }
+      );
 
-    connectedCallback() {
-        this.setAttribute('role', 'dialog');
-        this["content-width"] = "100%";
-        this["content-max-width"] = "420px";
-        this["content-height"] = "auto";
-        this["close-on-click-modal"] = false;
+    this.addEventListener(
+      "close",
+      () => {
+        this.#dispatchBubblesEvent("cancel");
+      },
+      { once: true, signal: this.#abortController.signal }
+    );
+  }
 
-        super.connectedCallback();
-
-        this.assignedStyle(stylesheet);
-
-        this.#abortController = new AbortController();
-
-        this.#confirmButton.addEventListener('click', () => {
-            this.#dispatchBubblesEvent('confirm');
-        }, { once: true, signal: this.#abortController.signal });
-
-        this.#closeIcon.addEventListener('click', () => {
-            this.#dispatchBubblesEvent('cancel');
-        }, { once: true, signal: this.#abortController.signal });
-
-        this.addEventListener('close', () => {
-            this.#dispatchBubblesEvent('cancel');
-        }, { once: true, signal: this.#abortController.signal });
-    }
-
-    $beforeUnmounted() {
-        this.#abortController?.abort();
-    }
+  $beforeUnmounted() {
+    this.#abortController?.abort();
+  }
 }
 
-if (!window.customElements.get('ea-message-box')) {
-    window.customElements.define('ea-message-box', EaMessageBoxElement);
+if (!window.customElements.get("ea-message-box")) {
+  window.customElements.define("ea-message-box", EaMessageBoxElement);
 }
--- src/components/ea-message-box/utils/EaMessageBoxInstance.js
+++ src/components/ea-message-box/utils/EaMessageBoxInstance.js
@@ -32,108 +32,132 @@
 
 /** @type {MessageBoxOptions} */
 const defaultOptions = {
-    title: '',
-    dangerouslyUseHTMLString: false,
-    message: '',
-    type: "primary",
-    icon: '',
-    closeIcon: 'icon-cancel',
-    showClose: true,
+  title: "",
+  dangerouslyUseHTMLString: false,
+  message: "",
+  type: "primary",
+  icon: "",
+  closeIcon: "icon-cancel",
+  showClose: true,
 
-    lockScroll: true,
+  // lockScroll: true,
 
-    showCancelButton: true,
-    showConfirmButton: true,
-    cancelButtonText: 'Cancel',
-    confirmButtonText: 'OK',
-    closeOnClickModal: true,
-    closeOnPressEscape: true,
+  showCancelButton: false,
+  showConfirmButton: false,
+  cancelButtonText: "Cancel",
+  confirmButtonText: "OK",
+  closeOnClickModal: false,
+  //   closeOnPressEscape: true,
 
-    showInput: false,
-    inputPlaceholder: '',
-    inputType: 'text',
-    inputValue: '',
-    inputPattern: null,
-    inputValidator: null,
-    inputErrorMessage: '',
+  //   showInput: false,
+  //   inputPlaceholder: "",
+  //   inputType: "text",
+  //   inputValue: "",
+  //   inputPattern: null,
+  //   inputValidator: null,
+  //   inputErrorMessage: "",
 
-    center: false,
-    draggable: false,
-    roundButton: false,
-    buttonSize: 'default',
-    appendTo: document.body,
+  center: false,
+  draggable: false,
+  roundButton: false,
+  buttonSize: "default",
+  appendTo: document.body,
 };
 
 const appendToHandler = (el, appendTo) => {
-    if (appendTo instanceof HTMLElement) {
-        appendTo.appendChild(el);
-    } else {
-        const appendTo = document.querySelector(appendTo);
-        appendTo ? appendTo.appendChild(el) : document.body.appendChild(el);
-    }
-}
+  if (appendTo instanceof HTMLElement) {
+    appendTo.appendChild(el);
+  } else {
+    const appendTo = document.querySelector(appendTo);
+    appendTo ? appendTo.appendChild(el) : document.body.appendChild(el);
+  }
+};
 
 const renderer = (options) => {
-    const messageBox = document.createElement('ea-message-box');
-    for (const k in options) {
-        const key = k.replace(/([a-z0-9])([A-Z])/g, "$1-$2")
-            .replace(/([A-Z]+)([A-Z][a-z])/g, "$1-$2")
-            .toLowerCase();
+  const messageBox = document.createElement("ea-message-box");
+  for (const k in options) {
+    const key = k
+      .replace(/([a-z0-9])([A-Z])/g, "$1-$2")
+      .replace(/([A-Z]+)([A-Z][a-z])/g, "$1-$2")
+      .toLowerCase();
 
-        messageBox.setAttribute(key, options[k])
-    }
+    messageBox.setAttribute(key, options[k]);
+  }
 
-    appendToHandler(messageBox, options.appendTo);
+  appendToHandler(messageBox, options.appendTo);
 
-    return messageBox;
+  return messageBox;
 };
 
 /**
- * @param {MessageBoxOptions} options 
+ * @param {MessageBoxOptions} options
  * @param {'alert' | 'confirm' | 'prompt'} boxType
- * @returns 
+ * @returns
  */
 export const EaMessageBox = (options) => {
-    options = Object.assign({ boxType: options.boxType }, defaultOptions, options);
+  options = Object.assign(
+    { boxType: options.boxType },
+    defaultOptions,
+    options
+  );
 
-    const controller = new AbortController();
-    const messageBox = renderer(options);
-    messageBox.visible = true;
-    messageBox.addEventListener("closed", () => {
-        controller.abort();
-        messageBox.remove();
-    }, { signal: controller.signal });
+  const controller = new AbortController();
+  const messageBox = renderer(options);
+  messageBox.visible = true;
+  messageBox.addEventListener(
+    "closed",
+    () => {
+      controller.abort();
+      messageBox.remove();
+    },
+    { signal: controller.signal }
+  );
 
-    return new Promise((resolve, reject) => {
-        messageBox.addEventListener('confirm', (e) => {
-            resolve(e);
-            messageBox.hide();
-        }, { signal: controller.signal });
+  return new Promise((resolve, reject) => {
+    messageBox.addEventListener(
+      "confirm",
+      (e) => {
+        resolve(e);
+        messageBox.hide();
+      },
+      { signal: controller.signal }
+    );
 
-        messageBox.addEventListener('cancel', (e) => {
-            reject(e);
-            messageBox.hide();
-        }, { signal: controller.signal });
-    })
-}
+    messageBox.addEventListener(
+      "cancel",
+      (e) => {
+        reject(e);
+        messageBox.hide();
+      },
+      { signal: controller.signal }
+    );
+  });
+};
 
-EaMessageBox.alert = (message, title, options) => EaMessageBox({
+EaMessageBox.alert = (message, title, options) =>
+  EaMessageBox({
     message,
     title,
-    boxType: 'alert',
+    showConfirmButton: true,
+    boxType: "alert",
     ...options,
-});
+  });
 
-EaMessageBox.confirm = (message, title, options) => EaMessageBox({
+EaMessageBox.confirm = (message, title, options) =>
+  EaMessageBox({
     message,
     title,
-    boxType: 'confirm',
+    showConfirmButton: true,
+    showCancelButton: true,
+    closeOnClickModal: true,
+    boxType: "confirm",
     ...options,
-});
+  });
 
-EaMessageBox.prompt = (message, title, options) => EaMessageBox({
+EaMessageBox.prompt = (message, title, options) =>
+  EaMessageBox({
     message,
     title,
-    boxType: 'confirm',
+    boxType: "confirm",
     ...options,
-});
+  });
