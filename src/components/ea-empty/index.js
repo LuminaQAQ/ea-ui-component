ab8a29af0717bf5c5af2a97f7458249a4e796feb
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-09-29 22:23:26 +0800

    feat(ea-empty): 完成重构

--- src/components/ea-empty/index.js
+++ src/components/ea-empty/index.js
@@ -1,20 +1,48 @@
 import Base from "@components/Base.js";
 
 import stylesheet from "./index.scss?inline";
+import { emptyStatusSVG } from "./assets/emptyStatusSVG";
 
 export class EaEmpty extends Base {
   /** @type {HTMLElement} */
   #container;
+  /** @type {HTMLElement} */
+  #placeholder;
+  /** @type {HTMLElement} */
+  #description;
+  /** @type {HTMLElement} */
+  #bottom;
 
   static get observedAttributes() {
-    return [...super.observedAttributes];
+    return [...super.observedAttributes, "image", "image-size", "description"];
   }
 
   state = this.properties({
-    type: {
-      //   type: ,
+    image: {
+      type: String,
       default: "",
-      observer: (newVal) => {},
+      observer: (newVal) => {
+        this.#placeholder.innerHTML = `<img class="ea-empty__image" src="${newVal}" alt="empty image" part="image" />`;
+      },
+    },
+    "image-size": {
+      type: String,
+      default: "",
+      observer: (newVal) => {
+        if (!CSS.supports("--ea-empty-size", newVal))
+          return console.warn(
+            `[ea-empty] The size value ${newVal} is not supported.`
+          );
+
+        this.style.setProperty("--ea-empty-size", newVal);
+      },
+    },
+    description: {
+      type: String,
+      default: "",
+      observer: (newVal) => {
+        this.#description.textContent = newVal;
+      },
     },
   });
 
@@ -39,11 +67,24 @@ export class EaEmpty extends Base {
   $render() {
     this.shadowRoot.innerHTML = `
       <div class='ea-empty' part='container'>
-        <slot></slot>
+        <div class="ea-empty__placeholder" part="placeholder">
+            <slot name="image">
+                <section class="ea-empty__default">${emptyStatusSVG}</section>
+            </slot>
+        </div>
+        <div class="ea-empty__description" part="description">
+            <slot name="description">No Data</slot>
+        </div>
+        <div class="ea-empty__bottom" part="bottom">
+            <slot></slot>
+        </div>
       </div>
-        `;
+    `;
 
     this.#container = this.shadowRoot.querySelector(".ea-empty");
+    this.#placeholder = this.shadowRoot.querySelector(".ea-empty__placeholder");
+    this.#description = this.shadowRoot.querySelector(".ea-empty__description");
+    this.#bottom = this.shadowRoot.querySelector(".ea-empty__bottom");
   }
 
   connectedCallback() {
