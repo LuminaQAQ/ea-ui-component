23a803b9d67cf022a60231899499ff54fa25b968
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-10-24 20:14:00 +0800

    feat(ea-tour): 完成基本样式和功能

--- src/components/ea-tour/components/ea-tour/index.js
+++ src/components/ea-tour/components/ea-tour/index.js
@@ -6,14 +6,23 @@ import EaUtils from "@/utils/Utils";
 export class EaTour extends Base {
   /** @type {HTMLElement} */
   #container;
+  /** @type {HTMLElement} */
+  #hollow;
+  /** @type {HTMLElement} */
+  #topMask;
+  /** @type {HTMLElement} */
+  #rightMask;
+  /** @type {HTMLElement} */
+  #bottomMask;
+  /** @type {HTMLElement} */
+  #leftMask;
 
   #states = {
-    current: 0,
     isChildrenLoaded: false,
   };
 
   static get observedAttributes() {
-    return [...super.observedAttributes, "visible"];
+    return [...super.observedAttributes, "visible", "current", "gap"];
   }
 
   state = this.properties({
@@ -21,18 +30,37 @@ export class EaTour extends Base {
       type: Boolean,
       default: "",
       observer: async (newVal) => {
-        if (!this.#states.isChildrenLoaded) {
-          await Promise.all(
-            [...this.children].map(async (child) =>
-              EaUtils.EaElement.addAsyncEventListener(
-                child,
-                "ea-tour-step-ready"
-              )
-            )
-          );
+        this.current = 0;
+        this.updateContainerClasslist();
+      },
+    },
+    gap: {
+      type: Number,
+      default: 6,
+      observer: (newVal) => {},
+    },
+    current: {
+      type: Number,
+      default: 0,
+      observer: async (newVal) => {
+        /** @type {HTMLElement[]} */
+        const children = [...this.querySelectorAll("ea-tour-step")];
+        if (children.length === 0) return;
+        if (newVal < 0) {
+          return (this.current = 0);
+        } else if (newVal >= children.length) {
+          return (this.visible = false);
         }
 
-        this.updateContainerClasslist();
+        this.#updateHollowPosition(newVal);
+        this.#updateSwitchvisibleStatus(newVal);
+
+        children.forEach((child, index) => {
+          child.style.setProperty(
+            "--ea-tour-step-visible",
+            index === newVal ? "block" : "none"
+          );
+        });
       },
     },
   });
@@ -77,12 +105,11 @@ export class EaTour extends Base {
               <rect class="ea-tour__hollow" part="hollow" x="50" y="50" width="100px" height="100px" fill="black" />
             </mask>
           </defs>
-
           <rect class='ea-tour__mask' x="0" y="0" width="100%" height="100%" mask="url(#reverseMask)"></rect>
-          <rect class="ea-tour__divider" x="0" y="0" width="100%" height="50px"></rect>
-          <rect class="ea-tour__divider" x="150px" y="0" width="100%" height="100%"></rect>
-          <rect class="ea-tour__divider" x="0" y="150px" width="100%" height="100%"></rect>
-          <rect class="ea-tour__divider" x="0" y="0" width="50px" height="100%"></rect>
+          <rect class="ea-tour__divider top-mask" x="0" y="0" width="100%" height="50px"></rect>
+          <rect class="ea-tour__divider right-mask" x="150px" y="0" width="100%" height="100%"></rect>
+          <rect class="ea-tour__divider bottom-mask" x="0" y="150px" width="100%" height="100%"></rect>
+          <rect class="ea-tour__divider left-mask" x="0" y="0" width="50px" height="100%"></rect>
         </svg>
       </div>
       <div class="ea-tour__content">
@@ -91,7 +118,69 @@ export class EaTour extends Base {
     `;
 
     this.#container = this.shadowRoot.querySelector(".ea-tour");
+    this.#hollow = this.shadowRoot.querySelector(".ea-tour__hollow");
+    this.#topMask = this.shadowRoot.querySelector(".ea-tour__divider.top-mask");
+    this.#rightMask = this.shadowRoot.querySelector(
+      ".ea-tour__divider.right-mask"
+    );
+    this.#bottomMask = this.shadowRoot.querySelector(
+      ".ea-tour__divider.bottom-mask"
+    );
+    this.#leftMask = this.shadowRoot.querySelector(
+      ".ea-tour__divider.left-mask"
+    );
+
+    this.addEventListener("next", (e) => {
+      this.current++;
+    });
+    this.addEventListener("previous", (e) => {
+      this.current--;
+    });
+    this.addEventListener("finish", (e) => {
+      this.visible = false;
+    });
   }
+
+  /**
+   * 更新 提示容器 的位置
+   * @param {number} [current] 当前步骤
+   */
+  #updateHollowPosition = (current = this.current) => {
+    /** @type {HTMLElement[]} */
+    const children = [...this.querySelectorAll("ea-tour-step")];
+    const halfGap = this.gap / 2;
+
+    const targetSelector = children[current].getAttribute("target");
+    const target = document.querySelector(targetSelector);
+    if (!target)
+      return console.warn(
+        `[EaTour] target ${targetSelector} not found`,
+        children[current]
+      );
+
+    const { width, height, x, y, top, right, bottom, left } =
+      target.getBoundingClientRect();
+    // 更新 提示容器 的位置
+    children[current].style.top = `${top + height + 18}px`;
+
+    // 更新 穿透部分 的位置
+    this.#hollow.style.width = `${width + this.gap}px`;
+    this.#hollow.style.height = `${height + this.gap}px`;
+    this.#hollow.style.x = `${x - halfGap}px`;
+    this.#hollow.style.y = `${y - halfGap}px`;
+
+    // 更新 不可穿透的遮罩 的位置
+    this.#topMask.style.height = `${y - halfGap}px`;
+    this.#rightMask.style.x = `${right + halfGap}px`;
+    this.#bottomMask.style.y = `${bottom + halfGap}px`;
+    this.#leftMask.style.width = `${x - halfGap}px`;
+  };
+
+  #updateSwitchvisibleStatus = (current = this.current) => {
+    // if (current === 0) {
+    //   this.#previousBtn.style.display = "none";
+    // }
+  };
 
   connectedCallback() {
     super.connectedCallback();
