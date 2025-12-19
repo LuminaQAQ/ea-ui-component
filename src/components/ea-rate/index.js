299081229111a7f5383369c1c0f238841310773e
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2025-12-20 01:48:12 +0800

    refactor(ea-rate):  完成基本重构

--- src/components/ea-rate/index.js
+++ src/components/ea-rate/index.js
@@ -1,39 +1,69 @@
 import FormAssociatedBase from "@/core/FormBase";
 
 import stylesheet from "./index.scss?inline";
+import { EA_COMPONENT_SIZES } from "@/utils/Variables";
 
 export class EaRate extends FormAssociatedBase {
   /** @type {HTMLElement} */
   #container;
+
   /** @type {AbortController} */
   #abortController = new AbortController();
+  /** @type {AbortController} */
+  #hoverAbortController = new AbortController();
 
   static get observedAttributes() {
-    return [...super.observedAttributes, "label", "value", "max"];
+    return [
+      ...super.observedAttributes,
+      "label",
+      "value",
+      "max",
+      "size",
+      "readonly",
+      "disabled",
+    ];
   }
 
   state = this.properties({
     label: {
       type: String,
       default: "",
-      observer: newVal => {},
+      observer: () => {},
     },
     value: {
       type: Number,
       default: 0,
       observer: newVal => {
-        this.setValue(newVal);
+        const displayValue = Math.max(newVal - 1, 0);
+
+        this.setValue(displayValue);
+        this.#setRateStatus(displayValue);
       },
     },
     max: {
       type: Number,
       default: 5,
-      observer: newVal => {},
+      observer: () => {},
     },
-    type: {
-      // type: ,
+    size: {
+      type: EA_COMPONENT_SIZES,
       default: "",
-      observer: newVal => {},
+      observer: () => {
+        this.updateContainerClasslist();
+      },
+    },
+
+    readonly: {
+      type: Boolean,
+      default: false,
+      observer: () => {},
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      observer: () => {
+        this.updateContainerClasslist();
+      },
     },
   });
 
@@ -41,11 +71,19 @@ export class EaRate extends FormAssociatedBase {
     getSymbol: {
       props: true,
       type: Function,
-      default: (value, isSelected) =>
-        `<ea-icon icon="icon-star-empty"></ea-icon>`,
+      /**
+       * 获取图标
+       * @param {string} [value]
+       * @param {boolean} [isSelected]
+       * @returns {string}
+       */
+      default: () => `<ea-icon icon="icon-star" part="icon"></ea-icon>`,
       /** @param {Function} cb */
       observer: cb => {
-        if (cb) this.#container.innerHTML = this.#renderRateEl(cb);
+        if (!cb || typeof cb !== "function") return;
+
+        this.#container.innerHTML = this.#renderRateEl(cb);
+        this.#setRateStatus(this.value - 1);
       },
     },
   });
@@ -55,9 +93,15 @@ export class EaRate extends FormAssociatedBase {
    * @return {string} 属性值
    */
   updateContainerClasslist() {
-    const className = this.computedClasslist("ea-rate", {
-      // ['--' + this.type]: this.type,
-    });
+    const className = this.computedClasslist(
+      "ea-rate",
+      {
+        ["--" + this.size]: this.size,
+      },
+      {
+        disabled: this.disabled,
+      }
+    );
 
     this.#container.className = className;
 
@@ -80,6 +124,8 @@ export class EaRate extends FormAssociatedBase {
     this.#container = this.shadowRoot.querySelector(".ea-rate");
 
     this.#renderRateEl(this.getSymbol);
+
+    this.updateContainerClasslist();
   }
 
   /**
@@ -105,18 +151,117 @@ export class EaRate extends FormAssociatedBase {
     return tpl;
   };
 
+  /**
+   * 设置 rate 元素选中状态
+   * @param {Number} index
+   */
+  #setRateStatus = (index = this.value - 1) => {
+    const children = [...this.#container.children];
+
+    children.forEach((el, i) => {
+      el.classList.toggle("is-selected", i <= index);
+    });
+  };
+
+  /**
+   * 取消 rate 元素选中状态
+   * @param {Number} [currentValue]
+   */
+  #unsetRateStatus = (currentValue = this.value - 1) => {
+    /** @type {HTMLElement[]} */
+    const children = [...this.#container.children];
+
+    children.forEach((el, i) => {
+      el.classList.toggle("is-selected", i <= currentValue);
+    });
+  };
+
+  /**
+   * 触发 hover 事件
+   * @param {Number} [value]
+   * @param {HTMLElement} [target]
+   */
+  #emitHoverEvent = (value = this.value, target = null) => {
+    this.emit("hover", {
+      detail: {
+        value,
+        target,
+      },
+    });
+  };
+
+  /**
+   * 当鼠标移入父元素时，设置子元素的选中状态
+   */
+  #onMousemove = () => {
+    if (this.readonly || this.disabled) return;
+
+    this.#hoverAbortController?.abort();
+    this.#hoverAbortController = new AbortController();
+
+    const onMousemove = e => {
+      const target = e.target.closest(".ea-rate__symbol");
+      const children = [...this.#container.children];
+      const index = children.indexOf(target);
+
+      this.#setRateStatus(index);
+
+      if (target) this.#emitHoverEvent(index, target);
+    };
+
+    const onMouseout = () => {
+      const value = this.hasAttribute("value") ? this.value - 1 : null;
+      const target = this.children[value];
+
+      this.#unsetRateStatus();
+
+      this.#emitHoverEvent(value, target);
+    };
+
+    this.#container.addEventListener("mousemove", onMousemove, {
+      signal: this.#hoverAbortController.signal,
+    });
+    this.#container.addEventListener("mouseout", onMouseout, {
+      signal: this.#hoverAbortController.signal,
+    });
+  };
+
+  /**
+   * 通过监听父元素的点击事件，设置子元素的选中状态
+   */
+  #onClick = e => {
+    if (this.readonly || this.disabled) return;
+
+    const target = e.target.closest(".ea-rate__symbol");
+    const children = [...this.#container.children];
+    const index = children.indexOf(target);
+    const displayValue = index + 1;
+
+    if (this.value === displayValue) {
+      this.value = 0;
+      this.#unsetRateStatus(0);
+    } else {
+      this.value = displayValue;
+    }
+  };
+
   connectedCallback() {
     super.connectedCallback();
     this.#abortController?.abort();
     this.#abortController = new AbortController();
 
-    // this.#container.addEventListener("mousemove", e => {
-    //   console.log(e.target);
-    // });
+    this.#container.addEventListener("mouseover", this.#onMousemove, {
+      signal: this.#abortController.signal,
+    });
+
+    this.#container.addEventListener("click", this.#onClick, {
+      signal: this.#abortController.signal,
+    });
   }
 
   $beforeUnmounted() {
-    this.#abortController.abort();
+    this.#abortController?.abort();
+    this.#hoverAbortController?.abort();
   }
 }
 
