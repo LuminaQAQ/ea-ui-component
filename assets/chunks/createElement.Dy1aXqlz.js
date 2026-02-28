62be888f6149b78241346e9c00bba7093bc7a67d
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2026-03-01 02:00:11 +0800

    Updates

--- assets/chunks/createElement.Dy1aXqlz.js
+++
@@ -1,1 +0,0 @@
-const n=(t,r,e)=>{const a=document.createElement(t||"div");return a.className=r||"",e&&(Array.isArray(e)?e.forEach(s=>{a.appendChild(s)}):a.appendChild(e)),a};export{n as c};
---
+++ assets/chunks/timeout.Cw2pzw4m.js
@@ -0,0 +1,1 @@
+const l=(e,r=0)=>{let t=setTimeout(()=>{clearTimeout(t),t=null,e()},r);return t};export{l as r};
