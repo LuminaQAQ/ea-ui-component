4003f5a57ca7b6d782e51bffab71a8ca3c2c7dff
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2026-03-08 00:18:40 +0800

    feat(ea-text): 添加对插槽内容的字体大小和行高支持
    fix(ea-button): 修复图标显示问题并优化样式
    
    refactor(ea-text): 使用Promise重构渲染状态管理
    
    style: 格式化HTML测试文件
    
    test: 更新ea-text测试用例

--- src/components/ea-text/index.js
+++ src/components/ea-text/index.js
@@ -1,120 +1,134 @@
-import Base from '../Base.js'
+import Base from "@components/Base.js";
 
-import stylesheet from './index.scss?inline';
+import stylesheet from "./index.scss?inline";
 
 export class EaText extends Base {
-    static get observedAttributes() {
-        return ["title", "type", "size", "truncated", "line-clamp", "tag"];
-    }
+  /** @type {HTMLElement} */
+  #container;
 
-    /** @type {HTMLElement} */
-    #container;
+  static get observedAttributes() {
+    return ["title", "type", "size", "truncated", "line-clamp", "tag"];
+  }
 
-    /** 
-     * @typedef {Object} State
-     * @property {string} title - 标题
-     * @property {string} type - 文本类型
-     * @property {string} size - 文本大小
-     * @property {boolean} truncated - 文本是否截断
-     * @property {number} line-clamp - 截断的行数
-     * @property {string} tag - 文本标签 
-     */
-    /** @type {State} */
-    state = this.properties({
-        title: {
-            type: String,
-            default: '',
-            observer: (newVal) => { }
-        },
-        type: {
-            type: ['normal', 'primary', 'success', 'warning', 'danger', 'info'],
-            default: 'normal',
-            observer: (newVal) => {
-                this.#container.className = this.updateContainerClasslist()
-            }
-        },
-        size: {
-            type: ['large', 'medium', 'small'],
-            default: 'medium',
-            observer: (newVal) => {
-                this.#container.className = this.updateContainerClasslist()
-            }
-        },
-        truncated: {
-            type: Boolean,
-            default: false,
-            observer: (newVal) => {
-                this.title = this.innerText || '';
-                this.#container.className = this.updateContainerClasslist()
-            }
-        },
-        "line-clamp": {
-            type: Number,
-            default: 0,
-            observer: (newVal) => {
-                if (newVal > 0) this.style.setProperty('--ea-text-line-clamp', newVal)
-                this.#container.className = this.updateContainerClasslist()
-                this.title = this.innerText || '';
-            }
-        },
-        tag: {
-            type: String,
-            default: 'span',
-            observer: (newVal) => { }
+  #states = {
+    isOriginalRendered: false,
+  };
+
+  #isOriginalRenderedPromise = new Promise(resolve => {
+    this.#states = new Proxy(this.#states, {
+      set: (target, key, value) => {
+        if (key === "isOriginalRendered" && value) {
+          resolve(true);
         }
-    })
+        return Reflect.set(target, key, value);
+      },
+    });
+  });
 
-    /**
-     * 获取 classlist 列表
-     * @return {string} 属性值
-     */
-    updateContainerClasslist() {
-        return this.computedClasslist('ea-text',
-            {
-                ['--' + this.type]: this.type,
-                ['--' + this.size]: this.size,
-                ['--truncated']: this.truncated,
-                ['--line-clamp']: this["line-clamp"] > 0,
-            }
-        );
-    }
+  /**
+   * @typedef {Object} State
+   * @property {string} title - 标题
+   * @property {string} type - 文本类型
+   * @property {string} size - 文本大小
+   * @property {boolean} truncated - 文本是否截断
+   * @property {number} line-clamp - 截断的行数
+   * @property {string} tag - 文本标签
+   */
 
-    constructor() {
-        super();
+  /** @type {State} */
+  state = this.properties({
+    title: {
+      type: String,
+      default: "",
+      observer: newVal => {},
+    },
+    type: {
+      type: ["normal", "primary", "success", "warning", "danger", "info"],
+      default: "normal",
+      observer: async newVal => {
+        await this.#isOriginalRenderedPromise;
+        this.updateContainerClasslist();
+      },
+    },
+    size: {
+      type: ["large", "medium", "small"],
+      default: "medium",
+      observer: async newVal => {
+        await this.#isOriginalRenderedPromise;
+        this.updateContainerClasslist();
+      },
+    },
+    truncated: {
+      type: Boolean,
+      default: false,
+      observer: async newVal => {
+        await this.#isOriginalRenderedPromise;
+        this.title = this.innerText || "";
+        this.updateContainerClasslist();
+      },
+    },
+    "line-clamp": {
+      type: Number,
+      default: 0,
+      observer: async newVal => {
+        await this.#isOriginalRenderedPromise;
+        this.#container.style.setProperty("--ea-text-line-clamp", newVal);
+        this.title = this.innerText || "";
 
-        this.isMounted = false;
+        this.updateContainerClasslist();
+      },
+    },
+    tag: {
+      type: String,
+      default: "span",
+      observer: newVal => {},
+    },
+  });
 
-        this.stylesheet = stylesheet;
-    }
+  /**
+   * 获取 classlist 列表
+   * @return {string} 属性值
+   */
+  updateContainerClasslist() {
+    const className = this.computedClasslist("ea-text", {
+      ["--" + this.type]: this.type,
+      ["--" + this.size]: this.size,
+      ["--truncated"]: this.truncated,
+      ["--line-clamp"]: this["line-clamp"] > 0,
+    });
 
-    $render() {
-        this.shadowRoot.innerHTML = `
-            <${this.tag} class="ea-text" part="container">
-                <slot></slot>
-            </${this.tag}>
-        `;
+    this.#container.className = className;
 
-        this.#container = this.shadowRoot.querySelector('.ea-text')
+    return className;
+  }
 
-        this.#container.className = this.updateContainerClasslist()
+  constructor() {
+    super();
 
-        this.isMounted = true;
-    }
+    this.stylesheet = stylesheet;
+  }
 
-    connectedCallback() {
-        super.connectedCallback();
+  $render() {
+    this.shadowRoot.innerHTML = `
+      <${this.tag} class="ea-text" part="container">
+        <slot></slot>
+      </${this.tag}>
+    `;
 
-        this.$render();
+    this.#container = this.shadowRoot.querySelector(".ea-text");
 
-        this.tag = this.tag;
-        this.title = this.title;
-        this.type = this.type;
-        this.size = this.size;
-        this.truncated = this.truncated;
-        this["line-clamp"] = this['line-clamp'];
-    }
+    this.updateContainerClasslist();
+
+    this.#states.isOriginalRendered = true;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.$render();
+  }
 }
 
-if (!window.customElements.get('ea-text')) {
-    window.customElements.define('ea-text', EaText);
+if (!window.customElements.get("ea-text")) {
+  window.customElements.define("ea-text", EaText);
 }
