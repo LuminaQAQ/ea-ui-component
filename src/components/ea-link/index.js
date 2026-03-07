df206de119292b7dd967a57be49ec1dfc3c01459
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2026-03-08 00:46:05 +0800

    feat(ea-link): 更新图标渲染方式

--- src/components/ea-link/index.js
+++ src/components/ea-link/index.js
@@ -1,6 +1,6 @@
-import Base from '../Base.js'
+import Base from "../Base.js";
 
-import stylesheet from './index.scss?inline';
+import stylesheet from "./index.scss?inline";
 
 export class EaLink extends Base {
   static get observedAttributes() {
@@ -9,8 +9,10 @@ export class EaLink extends Base {
 
   /** @type {HTMLAnchorElement} */
   #container;
+  /** @type {HTMLElement} */
+  #icon;
 
-  /** 
+  /**
    * @typedef {Object} LinkState
    * @property {string} type
    * @property {Boolean} disabled
@@ -22,58 +24,57 @@ export class EaLink extends Base {
   /** @type {LinkState} */
   state = this.properties({
     type: {
-      type: ['normal', 'primary', 'success', 'info', 'warning', 'danger'],
-      default: 'normal',
-      observer: (newVal) => {
-        this.#container.className = this.updateContainerClasslist()
-      }
+      type: ["normal", "primary", "success", "info", "warning", "danger"],
+      default: "normal",
+      observer: newVal => {
+        this.#container.className = this.updateContainerClasslist();
+      },
     },
     disabled: {
       type: Boolean,
       default: false,
-      observer: (newVal) => {
-        this.#container.className = this.updateContainerClasslist()
-      }
+      observer: newVal => {
+        this.#container.className = this.updateContainerClasslist();
+      },
     },
     underline: {
-      type: ['always', 'hover', 'never'],
-      default: '',
-      observer: (newVal) => {
-        this.#container.className = this.updateContainerClasslist()
-      }
+      type: ["always", "hover", "never"],
+      default: "",
+      observer: newVal => {
+        this.#container.className = this.updateContainerClasslist();
+      },
     },
     href: {
       type: String,
-      default: '',
-      observer: (newVal) => {
+      default: "",
+      observer: newVal => {
         this.#container.href = newVal;
-      }
+      },
     },
     icon: {
       type: String,
-      default: '',
-      observer: (newVal) => {
-        if (!newVal) return;
-
-        const icon = document.createElement('ea-icon');
-        icon.icon = newVal;
-        icon.part = "icon";
-
-        this.#container.insertBefore(icon, this.#container.firstChild);
-      }
+      default: "",
+      observer: newVal => {
+        this.#icon.setAttribute("icon", newVal);
+        this.#container.className = this.updateContainerClasslist();
+      },
     },
-  })
+  });
 
   /**
    * 获取 classlist 列表
    * @return {string} 属性值
    */
   updateContainerClasslist() {
-    return this.computedClasslist('ea-link', {
-      ['--' + this.type]: this.type,
-      ['--underline-' + this.underline]: this.underline,
-      '--disabled': this.disabled,
-    });
+    return this.computedClasslist(
+      "ea-link",
+      {
+        ["--" + this.type]: this.type,
+        ["--underline-" + this.underline]: this.underline,
+        "--disabled": this.disabled,
+      },
+      { icon: this.icon?.length }
+    );
   }
 
   constructor() {
@@ -87,11 +88,13 @@ export class EaLink extends Base {
   $render() {
     this.shadowRoot.innerHTML = `
       <a class="ea-link" part="container" tabindex="-1">
+        <ea-icon class="ea-link__icon" part="icon"></ea-icon>
         <slot></slot>
       </a>
     `;
 
-    this.#container = this.shadowRoot.querySelector('.ea-link');
+    this.#container = this.shadowRoot.querySelector(".ea-link");
+    this.#icon = this.shadowRoot.querySelector(".ea-link__icon");
   }
 
   connectedCallback() {
